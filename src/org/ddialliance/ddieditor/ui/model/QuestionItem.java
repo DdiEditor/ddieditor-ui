1c1ec323e11b6d3ac2754eecc0e5a3838e04e614
Dan Kristiansen <dak@dda.dk>
2009-04-29 13:13:03 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@375 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/model/QuestionItem.java
+++ src/org/ddialliance/ddieditor/ui/model/QuestionItem.java
@@ -1,6 +1,6 @@
 package org.ddialliance.ddieditor.ui.model;
 
-import java.text.MessageFormat;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,21 +14,19 @@ import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.LiteralTextType;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.NumericDomainDocument;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.NumericDomainType;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.QuestionItemDocument;
-import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.QuestionItemType;
-import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.ResponseDomainDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.TextDomainDocument;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.TextType;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.impl.QuestionItemTypeImpl;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.NumericTypeCodeType;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.ReferenceType;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.RepresentationType;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.TextDomainType;
 import org.ddialliance.ddieditor.ui.dbxml.Util;
-import org.ddialliance.ddieditor.ui.editor.ResponseTypeDetail.RESPONSE_TYPES;
+import org.ddialliance.ddieditor.ui.editor.question.ResponseTypeDetail.RESPONSE_TYPES;
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.w3.x1999.xhtml.LanguageCode;
 
 public class QuestionItem {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, QuestionItem.class);
@@ -348,6 +346,66 @@ public class QuestionItem {
 	public RepresentationType getResponseDomain() {
 		return questionItemTypeImpl.getResponseDomain();
 	}
+	
+	/**
+	 * Set Question Item Code Response Domain
+	 * 
+	 * @param codeSchemeReference
+	 * @return RepresentationType
+	 */
+	public RepresentationType setCodeResponseDomain(String codeSchemeReference) {
+
+		if (questionItemTypeImpl.getResponseDomain() != null) {
+			questionItemTypeImpl.unsetResponseDomain();
+		}
+		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		CodeDomainType cdt = (CodeDomainType) rt.substitute(CodeDomainDocument.type.getDocumentElementName(),
+				CodeDomainType.type);
+		cdt.addNewCodeSchemeReference().addNewID().setStringValue(codeSchemeReference);
+		return cdt;
+	}
+
+	/**
+	 * Set Question Item Text Response Domain
+	 * 
+	 * @param maxLength
+	 * @return RepresentationType
+	 */
+	public RepresentationType setTextResponseDomain(BigInteger maxLength) {
+
+		if (questionItemTypeImpl.getResponseDomain() != null) {
+			questionItemTypeImpl.unsetResponseDomain();
+		}
+		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		TextDomainType tdt = (TextDomainType) rt.substitute(TextDomainDocument.type.getDocumentElementName(),
+				TextDomainType.type);
+		tdt.setMaxLength(maxLength);
+		return tdt;
+	}
+
+	/**
+	 * Set Question Item Numeric Response Domain
+	 * 
+	 * @param type (integer or float)
+	 * @param decimalPosition (not used if integer)
+	 * @return RepresentationType
+	 */
+	public RepresentationType setNumericResponseDomain(NumericTypeCodeType.Enum type, BigInteger decimalPosition) {
+
+		if (questionItemTypeImpl.getResponseDomain() != null) {
+			questionItemTypeImpl.unsetResponseDomain();
+		}
+		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		NumericDomainType ndt = (NumericDomainType) rt.substitute(NumericDomainDocument.type
+				.getDocumentElementName(), NumericDomainType.type);
+		ndt.setType(type);
+		if (type == NumericTypeCodeType.FLOAT) {
+			ndt.setDecimalPositions(decimalPosition);
+		}
+		return ndt;
+	}
+
+
 
 	/**
 	 * Set new Question Item Response Domain
@@ -370,12 +428,16 @@ public class QuestionItem {
 			cdt.addNewCodeSchemeReference().addNewID().setStringValue(value);
 			return cdt;
 		} else if (responseType == RESPONSE_TYPES.TEXT) {
-			// TODO Support Text.
-			return null;
+			TextDomainType tdt = (TextDomainType) rt.substitute(TextDomainDocument.type.getDocumentElementName(),
+					TextDomainType.type);
+			// TODO Support MaxLength:
+			// tdt.setMaxLength(arg0);
+			return tdt;
 		} else if (responseType == RESPONSE_TYPES.NUMERIC) {
 			NumericDomainType ndt = (NumericDomainType) rt.substitute(NumericDomainDocument.type
 					.getDocumentElementName(), NumericDomainType.type);
 			ndt.setType(null);
+			ndt.setDecimalPositions(null);
 			return ndt;
 		} else if (responseType == RESPONSE_TYPES.DATE) {
 			// TODO Support Date
@@ -399,6 +461,10 @@ public class QuestionItem {
 	 */
 	public String getId() {
 		return questionItemTypeImpl.getId();
+	}
+	
+	public boolean getIsVersionable() {
+		return questionItemTypeImpl.getIsVersionable();
 	}
 
 	/**
@@ -441,8 +507,7 @@ public class QuestionItem {
 			}
 		}
 
-		// No error found:
-		return;
+		return; // No error found:
 	}
 
 }
