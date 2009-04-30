af5ec5294851545017417cdac3deeff73e9e4cf2
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-04-30 12:13:53 +0000

    Change window size plus add ShowPerspectiveBar true
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@387 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/ApplicationWorkbenchWindowAdvisor.java
+++ src/org/ddialliance/ddieditor/ui/ApplicationWorkbenchWindowAdvisor.java
@@ -10,24 +10,36 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
-	private static Log log = LogFactory.getLog(LogType.SYSTEM, ApplicationWorkbenchWindowAdvisor.class);
-	
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-        log.debug("ApplicationWorkbenchWindowAdvisor.ApplicationWorkbenchWindowAdvisor()");
-    }
+	private static Log log = LogFactory.getLog(LogType.SYSTEM,
+			ApplicationWorkbenchWindowAdvisor.class);
 
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-    	log.debug("ApplicationWorkbenchWindowAdvisor.createActionBarAdvisor()");
-        return new ApplicationActionBarAdvisor(configurer);
-    }
-    
-    public void preWindowOpen() {
-    	log.debug("ApplicationWorkbenchWindowAdvisor.preWindowOpen()");
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
-        configurer.setShowCoolBar(true);
-        configurer.setShowStatusLine(true);
-        configurer.setTitle("DDI Editor");
-    }
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+		if (log.isDebugEnabled()) {
+			log
+					.debug("ApplicationWorkbenchWindowAdvisor.ApplicationWorkbenchWindowAdvisor()");
+		}
+	}
+
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		if (log.isDebugEnabled()) {
+			log
+					.debug("ApplicationWorkbenchWindowAdvisor.createActionBarAdvisor()");
+		}
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		if (log.isDebugEnabled()) {
+			log.debug("ApplicationWorkbenchWindowAdvisor.preWindowOpen()");
+		}
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(800, 600));
+		configurer.setShowCoolBar(true);
+		configurer.setShowStatusLine(true);
+		configurer.setTitle("DDI Editor");
+		configurer.setShowPerspectiveBar(true);
+	}
 }
