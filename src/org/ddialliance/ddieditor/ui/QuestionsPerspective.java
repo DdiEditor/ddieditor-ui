fbe8684b815f3f7d78cb5c7729cfd5a96eda91ad
Dan Kristiansen <dak@dda.dk>
2009-05-04 07:43:31 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@404 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/QuestionsPerspective.java
@@ -0,0 +1,30 @@
+package org.ddialliance.ddieditor.ui;
+
+import org.ddialliance.ddieditor.ui.editor.Editor;
+import org.ddialliance.ddieditor.ui.view.QuestionItemView;
+import org.ddialliance.ddieditor.ui.view.View;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class QuestionsPerspective implements IPerspectiveFactory {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM, QuestionsPerspective.class);
+	public static final String ID = "org.ddialliance.ddieditor.ui.QuestionsPerspective";
+
+	public void createInitialLayout(IPageLayout layout) {
+		log.debug("QuestionsPerspective.createInitialLayout()");
+		addViewShortcuts(layout);
+		layout.addView(QuestionItemView.ID, IPageLayout.LEFT, 0.78f, layout.getEditorArea());
+		addFastViews(layout);
+	}
+
+	private void addViewShortcuts(IPageLayout layout) {
+		System.currentTimeMillis();
+	}
+
+	private void addFastViews(IPageLayout layout) {
+		System.currentTimeMillis();
+	}
+}
