2371a1ec81b540291a0677a99e0418f110ce68ad
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-08 07:05:29 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@619 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/util/FixedIn31.java
@@ -0,0 +1,34 @@
+package org.ddialliance.ddieditor.ui.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.DescriptionDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.LabelDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.LabelType;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.StructuredStringType;
+import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
+
+/**
+ * TODO desolve references when moving to ddi-3.1
+ */
+public class FixedIn31 {
+	static final String text = "Label will be fixed in ddi-3.1";
+	public static List<LabelType> getLabelList() {
+		LabelType label = LabelDocument.Factory.newInstance().addNewLabel();
+		label.setStringValue(text);
+
+		List<LabelType> list = new ArrayList<LabelType>();
+		list.add(label);
+		return list;
+	}
+	
+	public static List<StructuredStringType> getDesrcList() {
+		StructuredStringType description = DescriptionDocument.Factory.newInstance().addNewDescription();
+		XmlBeansUtil.setTextOnMixedElement(description, text);
+		
+		List<StructuredStringType> list = new ArrayList<StructuredStringType>();
+		list.add(description);
+		return list;
+	}
+}
