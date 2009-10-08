f0babd8826dc787bf3fcaac0932ba6b1bccc0264
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-08 07:09:49 +0000

    Initial import Subject of change or removal
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@627 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/wizard/export/ExportDdiXmlWizardPage.java
@@ -0,0 +1,26 @@
+package org.ddialliance.ddieditor.ui.wizard.export;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.dialogs.WizardExportResourcesPage;
+
+public class ExportDdiXmlWizardPage extends WizardExportResourcesPage {
+
+	protected ExportDdiXmlWizardPage(String pageName,
+			IStructuredSelection selection) {
+		super(pageName, selection);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void createDestinationGroup(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// TODO Auto-generated method stub		
+	}
+}
