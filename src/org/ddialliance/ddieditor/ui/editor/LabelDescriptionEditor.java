445d34530faa03359ce81486ad411067d82e4cd8
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-08 07:15:35 +0000

    Refactoring of SimpleEditor
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@637 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/LabelDescriptionEditor.java
@@ -0,0 +1,75 @@
+package org.ddialliance.ddieditor.ui.editor;
+
+/**
+ * Generic Simple Editor (support for Label and Description).
+ * 
+ */
+/*
+ * $Author$ 
+ * $Date$ 
+ * $Revision$
+ */
+
+import org.ddialliance.ddieditor.ui.model.LabelDescription;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+
+public class LabelDescriptionEditor extends Editor {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM,
+			LabelDescriptionEditor.class);
+
+	// Member variables:
+	private String editorEntityName;
+	private LabelDescription simpleElement;
+	private IEditorSite site;
+	private TableViewer tableViewer;
+
+	public LabelDescriptionEditor(String headerEditorTitle, String headerEditorDescr,
+			String editorEntityName) {
+
+		super(headerEditorTitle, headerEditorDescr);
+
+		this.editorEntityName = editorEntityName;
+	}
+
+	/**
+	 * Create contents of the editor part
+	 * 
+	 * @param parent
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout());
+		log.debug("Editor.createPartControl called");
+		super.createPartControl(parent);
+
+		createTabFolder(getComposite_1());
+		createLabelDescriptionTab(getTabFolder(), editorEntityName,
+				simpleElement);
+		createPropertiesTab(getTabFolder());
+
+		// Clean dirt from initialization
+		editorStatus.clearChanged();
+	}
+
+	public Viewer getViewer() {
+		log.debug("SimpleEditor.getViewer()");
+		return this.tableViewer;
+	}
+
+	// @Override
+	public void init(IEditorSite site, IEditorInput input, LabelDescription simpleElement)
+			throws PartInitException {
+		this.simpleElement = simpleElement;
+		// Initialize the Stander Editor Part:
+		super.init(site, input);
+	}
+}
