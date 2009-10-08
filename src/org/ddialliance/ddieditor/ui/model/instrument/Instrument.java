373aa8db091b89b3e3b622e6270cddbc7b6c8688
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-08 07:11:26 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@631 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/model/instrument/Instrument.java
@@ -0,0 +1,29 @@
+package org.ddialliance.ddieditor.ui.model.instrument;
+
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.InstrumentDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.InstrumentType;
+import org.ddialliance.ddieditor.ui.model.LabelDescription;
+import org.ddialliance.ddieditor.ui.util.FixedIn31;
+
+public class Instrument extends LabelDescription {
+	private InstrumentDocument doc;
+	private InstrumentType type;
+
+	public Instrument(InstrumentDocument doc, String parentId,
+			String parentVersion) throws Exception {
+		super(doc.getInstrument().getId(), doc.getInstrument().getVersion(),
+				parentId, parentVersion, FixedIn31.getLabelList(), FixedIn31.getDesrcList());
+
+		if (doc == null) {
+			this.doc = InstrumentDocument.Factory.newInstance();
+			
+		} else {
+			this.doc = doc;			
+		}
+		this.type = doc.getInstrument();
+	}
+	
+	public InstrumentDocument getDoc() {
+		return doc;
+	}
+}
---
+++ src/org/ddialliance/ddieditor/ui/model/instrument/StatementItem.java
@@ -0,0 +1,28 @@
+package org.ddialliance.ddieditor.ui.model.instrument;
+
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.StatementItemDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.StatementItemType;
+import org.ddialliance.ddieditor.ui.model.Model;
+
+public class StatementItem extends Model {
+	private StatementItemDocument doc;
+	private StatementItemType type;
+
+	public StatementItem(StatementItemDocument doc, String parentId,
+			String parentVersion) throws Exception {
+		super(doc.getStatementItem().getId(), doc.getStatementItem()
+				.getVersion(), parentId, parentVersion);
+
+		if (doc == null) {
+			this.doc = StatementItemDocument.Factory.newInstance();
+
+		} else {
+			this.doc = doc;
+		}
+		this.type = doc.getStatementItem();
+	}
+
+	public StatementItemDocument getDoc() {
+		return doc;
+	}
+}
