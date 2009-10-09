70e8ae06a08e6cf915e6d35e9bf28f98c6c8c2f1
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-09 11:04:01 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@661 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/dbxml/instrument/InstrumentDao.java
@@ -0,0 +1,157 @@
+package org.ddialliance.ddieditor.ui.dbxml.instrument;
+
+import java.util.List;
+
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.InstrumentDocument;
+import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.InstrumentType;
+import org.ddialliance.ddieditor.model.DdiManager;
+import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
+import org.ddialliance.ddieditor.ui.dbxml.IDao;
+import org.ddialliance.ddieditor.ui.dbxml.XmlEntities;
+import org.ddialliance.ddieditor.ui.model.IModel;
+import org.ddialliance.ddieditor.ui.model.instrument.Instrument;
+import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+
+public class InstrumentDao extends XmlEntities implements IDao {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM,
+			InstrumentDao.class);
+	
+	/**
+	 * Get Light XML Object
+	 * 
+	 * @param parentInstrumentScheme
+	 * @return List<LightXmlObjectType>
+	 * @throws Exception
+	 */
+	public List<LightXmlObjectType> getLightXmlObject(
+			LightXmlObjectType parentInstrumentScheme) throws Exception {
+
+		log.debug("Instruments.getInstrumentsLight()");
+
+		return getLightXmlObject("", "", parentInstrumentScheme.getId(),
+				parentInstrumentScheme.getVersion());
+	}
+
+	/**
+	 * Get Light XML Object
+	 * 
+	 * @param id
+	 * @param version
+	 * @param parentId
+	 * @param parentVersion
+	 * @return List<LightXmlObjectType>
+	 * @throws Exception
+	 */
+	public List<LightXmlObjectType> getLightXmlObject(String id,
+			String version, String parentId, String parentVersion)
+			throws Exception {
+
+		log.debug("Instruments.getInstrumentsLight()");
+
+		List<LightXmlObjectType> lightXmlObjectTypeList = DdiManager
+				.getInstance().getInstrumentsLight(id, version, parentId,
+						parentVersion).getLightXmlObjectList()
+				.getLightXmlObjectList();
+
+		return lightXmlObjectTypeList;
+	}
+
+	/**
+	 * Create model
+	 * 
+	 * @param id
+	 * @param version
+	 * @param parentId
+	 * @param parentVersion
+	 * @return Instrument
+	 * @throws Exception
+	 */
+	public Instrument create(String id, String version, String parentId,
+			String parentVersion) throws Exception {
+		InstrumentDocument doc = InstrumentDocument.Factory.newInstance();
+		InstrumentType type = doc.addNewInstrument();
+		type.setId(id);
+		if (version != null) {
+			type.setVersion(version);
+		}
+
+		Instrument model = new Instrument(doc, parentId, parentVersion);
+		return model;
+	}
+
+	/**
+	 * Get model
+	 * 
+	 * @param id
+	 * @param version
+	 * @param parentId
+	 * @param parentVersion
+	 * @return Instrument
+	 * @throws Exception
+	 */
+	public Instrument getModel(String id, String version, String parentId,
+			String parentVersion) throws Exception {
+		log.debug("Instruments.getInstrument(" + id + ")");
+
+		InstrumentDocument doc = DdiManager.getInstance().getInstrument(id,
+				version, parentId, parentVersion);
+		Instrument model = new Instrument(doc, parentId, parentVersion);
+
+		return model;
+	}
+
+	/**
+	 * Create new DBXML
+	 * 
+	 * @param model
+	 *            model
+	 * @param parentId
+	 *            parent id
+	 * @param parentVersion
+	 *            parent version
+	 * @throws DDIFtpException
+	 */
+	public void create(IModel model) throws DDIFtpException {
+		DdiManager.getInstance().createElement(model.getDocument(),
+				model.getParentId(), model.getParentVersion(), "datacollection__DataCollection");
+	}
+
+	/**
+	 * 
+	 * Update DBXML
+	 * 
+	 * @param model
+	 *            model
+	 * @throws DDIFtpException
+	 */
+	public void update(IModel model) throws DDIFtpException {
+		DdiManager.getInstance().updateElement(model.getDocument(),
+				model.getId(), model.getVersion());
+	}
+
+	/**
+	 * 
+	 * Delete DBXML
+	 * 
+	 * @param id
+	 *            Identification
+	 * @param version
+	 *            Version
+	 * @param parentId
+	 *            Parent Identification
+	 * @param parentVersion
+	 *            Parent Version
+	 * @throws Exception
+	 */
+	public void delete(String id, String version, String parentId,
+			String parentVersion) throws Exception {
+		log.debug("Delete DBXML");
+		IModel  model = getModel(id, version, parentId, parentVersion);
+		DdiManager.getInstance()
+				.deleteElement(model.getDocument(), model.getParentId(),
+						model.getParentVersion(), "datacollection__DataCollection");
+	}
+}
