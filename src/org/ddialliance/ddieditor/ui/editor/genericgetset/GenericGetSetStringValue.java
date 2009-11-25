de676febb415c44e9ca5531836893baad4f4b6d5
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-11-25 08:16:03 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@749 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/genericgetset/GenericGetSetStringValue.java
@@ -0,0 +1,31 @@
+package org.ddialliance.ddieditor.ui.editor.genericgetset;
+
+/**
+ * Definition of complex get and setStringValue on any element
+ */
+public interface GenericGetSetStringValue {
+	public static String GET_STRING_METHOD_NAME = "getStringValue";
+	public static String SET_STRING_METHOD_NAME = "setStringValue";
+	public static String GET_OBJECT_METHOD_NAME = "getObject";
+	
+	/**
+	 * Get the editorial value on any element
+	 * @param obj to perform complex get value on 
+	 * @return value
+	 */
+	public String getStringValue(Object obj);
+
+	/**
+	 * Set the editorial value on any element
+	 * @param obj to perform complex set value on
+	 * @param text value to set
+	 */
+	public void setStringValue(Object obj, Object text);
+
+	/**
+	 * Get editorial element  
+	 * @param obj to get element from
+	 * @return editorial element
+	 */
+	public Object getObject(Object obj);
+}
