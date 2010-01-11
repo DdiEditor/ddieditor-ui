de3501ed886ffb31f7acd5337662d1fed145eabd
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-01-11 09:37:29 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@832 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/model/ModelAttributes.java
@@ -0,0 +1,8 @@
+package org.ddialliance.ddieditor.ui.model;
+
+/**
+ * List of defined constants used to identify CRUD on element attributes
+ */
+public enum ModelAttributes {
+	ProgrammingLanguage;
+}
