2ec92c1598aa25ac2dd8e704f7f0e0f162ace82a
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-01-11 09:37:47 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@833 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/widgetutil/genericmodifylistener/GenericModifyListener.java
@@ -0,0 +1,32 @@
+package org.ddialliance.ddieditor.ui.editor.widgetutil.genericmodifylistener;
+
+import org.ddialliance.ddieditor.ui.editor.Editor.EditorIdentification;
+import org.ddialliance.ddieditor.ui.model.IModel;
+import org.ddialliance.ddieditor.ui.util.DialogUtil;
+import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.eclipse.swt.events.ModifyListener;
+
+public abstract class GenericModifyListener implements ModifyListener {
+	IModel model;
+	Class<?> modifyClass;
+
+	EditorIdentification editorIdentification;
+
+	GenericModifyListener(IModel model, Class<?> modifyClass,
+			EditorIdentification editorIdentification) {
+		super();
+		this.model = model;
+		this.modifyClass = modifyClass;
+		this.editorIdentification = editorIdentification;
+	}
+
+	void applyChange(Object value, Class<?> type) {
+		try {
+			model.applyChange(value, type);
+		} catch (Exception e) {
+			DDIFtpException e1 = new DDIFtpException(e);
+			DialogUtil.errorDialog(editorIdentification.getSite(),
+					editorIdentification.getID(), e1.getMessage(), e1);
+		}
+	}
+}
