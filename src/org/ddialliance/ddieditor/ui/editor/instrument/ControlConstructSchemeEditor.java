8aaa5499f9f055dda43f1e82b270ddb46da6503b
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-01-20 11:36:41 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@850 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/instrument/ControlConstructSchemeEditor.java
@@ -0,0 +1,11 @@
+package org.ddialliance.ddieditor.ui.editor.instrument;
+
+import org.ddialliance.ddieditor.ui.editor.Editor;
+
+public class ControlConstructSchemeEditor extends Editor {
+
+	public ControlConstructSchemeEditor() {
+		super("", "");
+	}
+
+}
