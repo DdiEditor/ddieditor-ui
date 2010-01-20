db7468025083d119f92bd1d1a7026dabd3d2f5a9
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-01-20 11:43:06 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@862 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/util/DialogUtil.java
+++ src/org/ddialliance/ddieditor/ui/util/DialogUtil.java
@@ -1,15 +1,59 @@
 package org.ddialliance.ddieditor.ui.util;
 
 import org.ddialliance.ddieditor.ui.view.Messages;
+import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorSite;
 
 public class DialogUtil {
-	public static void errorDialog(IEditorSite site, String editorId, String errorMessage, Throwable e) {
-		ErrorDialog.openError(site.getShell(), Messages
-				.getString("ErrorTitle"), null, new Status(
-				IStatus.ERROR, editorId, 0, errorMessage, e));
+	/**
+	 * Construct error dialog
+	 * 
+	 * @param site
+	 *            interface between an editor part and the workbench
+	 * @param pluginId
+	 *            ID of plug in e.g. editor, view, perspective ...
+	 * @param errorMessage
+	 *            error message to display
+	 * @param e
+	 *            exception or throwable
+	 */
+	public static void errorDialog(IEditorSite site, String pluginId, String title,
+			String errorMessage, Throwable e) {
+		errorDialog(site.getShell(), pluginId, title, errorMessage, e);
+	}
+
+	/**
+	 * Construct error dialog
+	 * 
+	 * @param shell
+	 *            parent shell
+	 * 
+	 * @param pluginId
+	 *            ID of plug in e.g. editor, view, perspective ...
+	 * @param errorMessage
+	 *            error message to display
+	 * @param e
+	 *            exception or throwable
+	 */
+	public static void errorDialog(Shell shell, String pluginId, String title, 
+			String errorMessage, Throwable e) {
+		checkException(e);
+		if (title==null) {
+			title = Messages.getString("ErrorTitle");
+		}
+		
+		ErrorDialog.openError(shell, title, null,
+				new Status(IStatus.ERROR, pluginId, 0, errorMessage, e));
+	}
+
+	private static void checkException(Throwable e) {
+		if (!(e instanceof DDIFtpException)) {
+			// construct a ddi ftp exception for logging purposes
+			new DDIFtpException(new Exception(e));
+		}
 	}
 }
