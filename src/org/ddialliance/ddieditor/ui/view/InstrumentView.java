723602b4478ce6981ce7f212c19cd89c94fdd2ce
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-01-25 11:42:09 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@877 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/view/InstrumentView.java
+++ src/org/ddialliance/ddieditor/ui/view/InstrumentView.java
@@ -3,25 +3,22 @@ package org.ddialliance.ddieditor.ui.view;
 import java.util.Arrays;
 
 import org.ddialliance.ddieditor.ui.model.ElementType;
-import org.eclipse.swt.widgets.Composite;
 
 public class InstrumentView extends View {
 	public static final String ID = "org.ddialliance.ddieditor.ui.view.InstrumentView";
 
 	public InstrumentView() {
-		super(ViewContentType.InstrumentationContent, Messages
-				.getString("InstrumentView.label.navigation"), Messages
-				.getString("InstrumentView.label.label.Instrument"), Messages
-				.getString("InstrumentView.label.selectLabel.NavigatorDescription"),
+		super(
+				ViewContentType.InstrumentationContent,
+				Messages.getString("InstrumentView.label.navigation"),
+				Messages.getString("InstrumentView.label.label.Instrument"),
+				Messages
+						.getString("InstrumentView.label.selectLabel.NavigatorDescription"),
 				ElementType.CONTROL_CONSTRUCT_SCHEME, "", Arrays.asList(
 						ElementType.IF_THEN_ELSE, ElementType.REPEAT_UNTIL,
 						ElementType.REPEAT_WHILE, ElementType.LOOP,
 						ElementType.SEQUENCE, ElementType.COMPUTATION_ITEM,
 						ElementType.STATEMENT_ITEM,
 						ElementType.QUESTION_CONSTRUCT));
-	}
-
-	public void createPartControl(Composite parent) {
-		super.createPartControl(parent);
 	}
 }
