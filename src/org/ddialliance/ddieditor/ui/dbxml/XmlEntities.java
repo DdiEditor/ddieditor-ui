6c64ab755e50281eb76dbd38cf0f24759d6099d4
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-03-01 11:34:45 +0000

    Removed init method, not used
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@967 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/dbxml/XmlEntities.java
+++ src/org/ddialliance/ddieditor/ui/dbxml/XmlEntities.java
@@ -13,12 +13,5 @@ package org.ddialliance.ddieditor.ui.dbxml;
 import java.util.Properties;
 
 public class XmlEntities {
-
 	protected static String xml_export_filename = "";
-	
-	public static void init (Properties properties) {
-		
-		xml_export_filename = properties.getProperty("xml_export_filename", "");
-	}
-	
 }
