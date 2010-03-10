8db26b1037fcbfd09acea4ac4a471f69d001f2e0
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-03-10 13:39:32 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1010 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/widgetutil/tab/PropertyTabItemAction.java
@@ -0,0 +1,133 @@
+package org.ddialliance.ddieditor.ui.editor.widgetutil.tab;
+
+import javax.xml.namespace.QName;
+
+import org.ddialliance.ddieditor.logic.identification.IdentificationManager;
+import org.ddialliance.ddieditor.logic.identification.VersionInformation;
+import org.ddialliance.ddieditor.logic.urn.ddi.Urn2Util;
+import org.ddialliance.ddieditor.model.DdiManager;
+import org.ddialliance.ddieditor.ui.model.IModel;
+import org.ddialliance.ddieditor.ui.util.DialogUtil;
+import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.xml.Urn;
+import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class PropertyTabItemAction extends TabItemAction {
+	IModel model;
+	Shell shell;
+	Combo actionCombo;
+	StyledText urnText;
+	Text agencyText;
+	Text idText;
+	Text versionText;
+	Text versionResponsibilityText;
+	Text versionDate;
+	StyledText versionRationaleText;
+
+	public PropertyTabItemAction(String id, IModel model, Shell shell,
+			Combo actionCombo, StyledText urnText, Text agencyText,
+			Text idText, Text versionText, Text versionResponsibilityText,
+			Text versionDate, StyledText versionRationaleText) {
+		super(id);
+		this.model = model;
+		this.shell = shell;
+		this.actionCombo = actionCombo;
+		this.urnText = urnText;
+		this.agencyText = agencyText;
+		this.idText = idText;
+		this.versionText = versionText;
+		this.versionResponsibilityText = versionResponsibilityText;
+		this.versionDate = versionDate;
+		this.versionRationaleText = versionRationaleText;
+	}
+
+	@Override
+	public Object action() throws DDIFtpException {
+		boolean isMaintainable = false;
+		try {
+			isMaintainable = DdiManager.getInstance().getDdi3NamespaceHelper()
+					.isMaintainable(model.getDocument());
+		} catch (DDIFtpException e) {
+			DialogUtil.errorDialog(shell, id, "Maintainable check error", e
+					.getMessage(), e);
+		}
+
+		// action
+		// Combo actionCombo = createCombo(group, new String[] { "1", "2", "3"
+		// });
+
+		// urn
+		Urn urn = null;
+		String urnStr = "";
+		try {
+			QName qName = model.getDocument().schemaType()
+					.getDocumentElementName();
+			urn = Urn2Util.getUrn(qName.getLocalPart(), model.getId(), model
+					.getVersion(), model.getParentId(), model
+					.getParentVersion());
+			urnStr = urn.toUrnString();
+		} catch (DDIFtpException e) {
+			DialogUtil.errorDialog(shell, id, "URN error", e.getMessage(), e);
+		}
+		urnText.setText(urnStr);
+
+		// agency
+		if (isMaintainable) {
+			agencyText.setText(model.getAgency() == null ? (urn == null ? ""
+					: urn.getIdentifingAgency()) : model.getAgency());
+		}
+
+		// id
+		idText.setText(model.getId());
+
+		// version
+		boolean isVersionable = false;
+		try {
+			isVersionable = DdiManager.getInstance().getDdi3NamespaceHelper()
+					.isVersionable(model.getDocument());
+		} catch (DDIFtpException e) {
+			DialogUtil.errorDialog(shell, id, "Versionable check error", e
+					.getMessage(), e);
+		}
+		if (isVersionable) {
+			// version
+			versionText.setText(model.getVersion() == null ? "" : model
+					.getVersion());
+
+			VersionInformation versionInformation = null;
+			try {
+				versionInformation = IdentificationManager.getInstance()
+						.getVersionInformation(model.getDocument());
+			} catch (DDIFtpException e) {
+				DialogUtil.errorDialog(shell, id, "Error", e.getMessage(), e);
+			}
+
+			// version responsibility
+			versionResponsibilityText
+					.setText(versionInformation.versionResponsibility == null ? ""
+							: versionInformation.versionResponsibility);
+
+			// version date
+			String versionDateStr = "";
+			try {
+				versionDateStr = XmlBeansUtil.getXmlAttributeValue(model
+						.getDocument().xmlText(), "versionDate=\"");
+			} catch (DDIFtpException e) {
+				DialogUtil.errorDialog(shell, id, "Error", e.getMessage(), e);
+			}
+			versionDate.setText(versionDateStr);
+
+			// version rationale
+			versionRationaleText
+					.setText(versionInformation.versionRationaleList.isEmpty() ? ""
+							: versionInformation.versionRationaleList.get(
+									versionInformation.versionRationaleList
+											.size() - 1).getStringValue());
+		}
+		return null;
+	}
+}
