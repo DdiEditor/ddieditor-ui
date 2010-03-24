d9caab1a237e9ff3b918e8bd6f3b44a4926e3b15
Dan Kristiansen <dak@dda.dk>
2010-03-24 13:44:36 +0000

    Using new facilities in Editor class.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1044 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/model/question/QuestionItem.java
+++ src/org/ddialliance/ddieditor/ui/model/question/QuestionItem.java
@@ -12,29 +12,29 @@ package org.ddialliance.ddieditor.ui.model.question;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.CodeDomainDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.CodeDomainType;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.DynamicTextType;
-import org.ddialliance.ddi3.xml.xmlbeans.datacollection.LiteralTextDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.LiteralTextType;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.NumericDomainDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.NumericDomainType;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.QuestionItemDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.TextDomainDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.TextType;
-import org.ddialliance.ddi3.xml.xmlbeans.datacollection.impl.QuestionItemTypeImpl;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.NumericTypeCodeType;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.RepresentationType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.StructuredStringType;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.TextDomainType;
+import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
 import org.ddialliance.ddieditor.ui.editor.question.ResponseTypeDetail.RESPONSE_TYPES;
 import org.ddialliance.ddieditor.ui.model.Language;
 import org.ddialliance.ddieditor.ui.model.Model;
+import org.ddialliance.ddieditor.ui.model.ModelAccessor;
 import org.ddialliance.ddieditor.ui.view.Messages;
+import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
@@ -42,9 +42,7 @@ import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
 
 public class QuestionItem extends Model {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, QuestionItem.class);
-	private QuestionItemDocument questionItemDocument;
-	private QuestionItemTypeImpl questionItemTypeImpl;
-	private List<QuestionItemLiteralText> questionItemLiteralTextList = new ArrayList<QuestionItemLiteralText>();
+	private QuestionItemDocument doc;
 	private String originalLanguageCode = null;
 
 	/**
@@ -61,46 +59,13 @@ public class QuestionItem extends Model {
 		super(questionItemDocument.getQuestionItem(), parentId, parentVersion);
 		
 		if (questionItemDocument == null) {
-			// TODO Create new Question Item
-			this.questionItemDocument = null;
+			this.doc = QuestionItemDocument.Factory.newInstance();
+			// add id and version
+			setId("");
+			setVersion("");
+		} else {
+			this.doc = questionItemDocument;
 		}
-		this.questionItemDocument = questionItemDocument;
-		this.questionItemTypeImpl = (QuestionItemTypeImpl) questionItemDocument.getQuestionItem();
-
-		// Generate list of Question Item Literal Texts:
-		// - all QuestionText and LiteralText elements with same language is
-		// gathered in same QuestionItemLiteralText object.
-		String[] languageCodes = Language.getLanguageCodes();
-		for (int i = 0; i < languageCodes.length; i++) {
-			String languageCode = languageCodes[i];
-			StringBuilder str = new StringBuilder(256);
-			boolean firstElement = true;
-
-			List<DynamicTextType> dynamicTextTypeList = questionItemTypeImpl.getQuestionTextList();
-			// Loop over Question Item Text list:
-			for (Iterator iterator = dynamicTextTypeList.iterator(); iterator.hasNext();) {
-				DynamicTextType dynamicTextType = (DynamicTextType) iterator.next();
-				if (originalLanguageCode == null && !dynamicTextType.getTranslated()) {
-					originalLanguageCode = dynamicTextType.getLang();
-				}
-				if (dynamicTextType.getLang().equals(languageCode)) {
-					List<TextType> textTypeList = dynamicTextType.getTextList();
-					// Loop over Literal Text list
-					for (Iterator iterator2 = textTypeList.iterator(); iterator2.hasNext();) {
-						TextType textType = (TextType) iterator2.next();
-						if (!firstElement) {
-							str.append("\n");
-						} else {
-							firstElement = false;
-						}
-						str.append(XmlBeansUtil.getTextOnMixedElement(textType));
-					}
-					questionItemLiteralTextList.add(new QuestionItemLiteralText(str.toString(), languageCode,
-							dynamicTextType.getTranslated(), dynamicTextType.getTranslatable()));
-				}
-			}
-		}
-
 	}
 
 	/**
@@ -133,7 +98,7 @@ public class QuestionItem extends Model {
 	 */
 	public String getConceptRef() throws Exception {
 		String conceptRef = "";
-		List<ReferenceType> conceptReferenceList = questionItemTypeImpl.getConceptReferenceList();
+		List<ReferenceType> conceptReferenceList = doc.getQuestionItem().getConceptReferenceList();
 		if (conceptReferenceList.size() == 1) {
 			conceptRef = XmlBeansUtil.getTextOnMixedElement(conceptReferenceList.get(0));
 			log.debug("ConceptRef: " + conceptRef);
@@ -142,7 +107,17 @@ public class QuestionItem extends Model {
 		}
 		return conceptRef;
 	}
-
+	
+	public ReferenceType getConceptReferenceType() {
+		List<ReferenceType> conceptReferenceList = doc.getQuestionItem().getConceptReferenceList();
+		if (conceptReferenceList.isEmpty()) {
+			ReferenceType ref = doc.getQuestionItem().addNewConceptReference();
+			ref.addNewID();
+			return ref;
+		}
+		return conceptReferenceList.get(0);
+	}
+	
 	/**
 	 * Set Concept reference
 	 * 
@@ -151,165 +126,60 @@ public class QuestionItem extends Model {
 	 */
 	public void setConceptRef(String conceptRef) throws Exception {
 
-		if (questionItemTypeImpl.getConceptReferenceArray().length > 1) {
+		if (doc.getQuestionItem().getConceptReferenceList().size() > 1) {
 			throw new Exception(Messages.getString("QuestionItem.mess.MultipleConceptReferencesNotSupported")); //$NON-NLS-1$
 		}
-		if (questionItemTypeImpl.getConceptReferenceArray().length == 1) {
-			questionItemTypeImpl.removeConceptReference(0);
+
+		if (doc.getQuestionItem().getConceptReferenceList().size() == 1) {
+			doc.getQuestionItem().removeConceptReference(0);
 		}
-		questionItemTypeImpl.addNewConceptReference().addNewID().setStringValue(conceptRef);
+		doc.getQuestionItem().addNewConceptReference().addNewID().setStringValue(conceptRef);
 	}
 
 	/**
-	 * Get text of Question Item corresponding to the given language code.
+	 * Get text of Question Item corresponding to default (original) language.
 	 * 
-	 * @param translated
-	 * @param languageCode
-	 *            - only used if translated = true
 	 * @return String
-	 * @throws Exception
+	 * @throws DDIFtpException
 	 */
-	public String getText(boolean translated, String languageCode) throws Exception {
+	public List<StructuredStringType> getText() {
 
-		for (Iterator iterator = questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText = (QuestionItemLiteralText) iterator.next();
-
-			if (translated == false && questionItemLiteralText.getTranslated() == translated) {
-				return questionItemLiteralText.getText();
+		boolean found = false;
+		DynamicTextType dynamicText = null;
+		List<DynamicTextType> dynamicTextList = doc.getQuestionItem().getQuestionTextList();
+		for (int i = 0; i < dynamicTextList.size(); i++) {
+			dynamicText = dynamicTextList.get(i);
+			if (dynamicText == null) {
+				return null;
 			}
-			if (translated == true && questionItemLiteralText.getTranslated() == translated
-					&& questionItemLiteralText.getLanguageCode().equals(languageCode)) {
-				return questionItemLiteralText.getText();
+			if (XmlBeansUtil.isDefaultLangElement(dynamicText)) {
+				found = true;
+				break;
 			}
 		}
-		return "";
-	}
+		if (!found) {
+			return null;
+		}
 
-	/**
-	 * Get QuestionItemLiteralText of Question Item corresponding to the given language code.
-	 * 
-	 * @param translated
-	 * @param languageCode
-	 *            - only used if translated = true
-	 * @return QuestionItemLiteralText
-	 * @throws Exception
-	 */
-	public QuestionItemLiteralText getQuestionItemLiteralText(boolean translated, String languageCode) throws Exception {
-
-		for (Iterator iterator = questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText = (QuestionItemLiteralText) iterator.next();
-
-			if (translated == false && questionItemLiteralText.getTranslated() == translated) {
-				return questionItemLiteralText;
+		List<LiteralTextType> LiteralTextList = new ArrayList<LiteralTextType>();		
+		for (TextType test : dynamicText.getTextList()) {
+			if (test instanceof LiteralTextType) {
+				LiteralTextList.add((LiteralTextType) test);
+			} else {
+				// Conditional Text Type
+				log.debug("Conditional Text Type currently not supported");
+				return null;
 			}
-			if (translated == true && questionItemLiteralText.getTranslated() == translated
-					&& questionItemLiteralText.getLanguageCode().equals(languageCode)) {
-				return questionItemLiteralText;
+		}
+
+		List<StructuredStringType> StructuredStringTypeList = new ArrayList<StructuredStringType>();
+		for (LiteralTextType literalTextType : LiteralTextList) {
+			if (literalTextType.getText() != null) {
+				StructuredStringTypeList.add(literalTextType.getText());
+				return StructuredStringTypeList;
 			}
 		}
 		return null;
-	}
-
-	/**
-	 * 
-	 * Create/Update Question Item text string corresponding to the given
-	 * language
-	 * 
-	 * Note: All GUI text lines are inserted into same Question Text element
-	 * with one Literal Text sub-element per text line.
-	 * 
-	 * @param newText
-	 * @param languageCode
-	 */
-	public void setText(String newText, String languageCode) {
-		boolean found = false;
-
-		for (Iterator iterator = questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText = (QuestionItemLiteralText) iterator.next();
-
-			if (questionItemLiteralText.getLanguageCode().equals(languageCode)) {
-				questionItemLiteralText.setText(newText);
-				found = true;
-			}
-		}
-
-		if (!found) {
-			// QuestionItemLiteralText not found - create it:
-			// TODO Translatable control by check-flag on Translated Question
-			// Tab
-			this.addText(newText, languageCode, false, false);
-		}
-	}
-
-	/**
-	 * 
-	 * Update QuestionItemDocument with Question Item Text elements. This method
-	 * has to be called before the Question Item is saved to the DBXML.
-	 * 
-	 */
-	public void update() {
-
-		// Remove old Question Text elements:
-		int length = questionItemTypeImpl.getQuestionTextList().size();
-		for (int i = 0; i < length; i++) {
-			questionItemTypeImpl.removeQuestionText(0);
-		}
-
-		// Loop over Question Text list:
-		for (Iterator iterator = this.questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText = (QuestionItemLiteralText) iterator.next();
-
-			// Create a Question Text element:
-			DynamicTextType questionText = questionItemTypeImpl.addNewQuestionText();
-			questionText.setLang(questionItemLiteralText.getLanguageCode());
-			questionText.setTranslatable(questionItemLiteralText.getTranslatable());
-			questionText.setTranslated(questionItemLiteralText.getTranslated());
-
-			// Add one Literal Text element per GUI text line
-			String newText = questionItemLiteralText.getText();
-			StringTokenizer st = new StringTokenizer(newText, "\n");
-			while (st.hasMoreTokens()) {
-				String substr = (String) st.nextElement();
-				TextType textType = questionText.addNewText();
-				LiteralTextType literalText = (LiteralTextType) textType.substitute(LiteralTextDocument.type
-						.getDocumentElementName(), LiteralTextType.type);
-				// TODO 3.1 upgrade
-				//literalText.setText(substr);
-			}
-		}
-		return;
-	}
-
-	/**
-	 * Add QuestionItemLiteralText element to questionItemLiteralTextList.
-	 * 
-	 * @param questionItemLiteralText
-	 */
-	public QuestionItemLiteralText addText(String newText, String languageCode, boolean translated, boolean translatable) {
-		QuestionItemLiteralText questionItemLiteralText = new QuestionItemLiteralText(newText, languageCode,
-				translated, translatable); 
-		questionItemLiteralTextList.add(questionItemLiteralText);
-		return questionItemLiteralText;
-
-	}
-
-	/**
-	 * Remove QuestionItemLiteralText list element from
-	 * questionItemLiteralTextList;
-	 * 
-	 * @param questionItemLiteralText
-	 */
-	public void removeText(QuestionItemLiteralText questionItemLiteralText) {
-		int i = 0;
-		for (Iterator iterator = questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText1 = (QuestionItemLiteralText) iterator.next();
-
-			if (questionItemLiteralText.getLanguageCode().equals(questionItemLiteralText1.getLanguageCode())) {
-				questionItemLiteralTextList.remove(i);
-				return;
-			}
-			i++;
-		}
 	}
 
 	/**
@@ -318,7 +188,7 @@ public class QuestionItem extends Model {
 	 * @return RepresentationType
 	 */
 	public RepresentationType getResponseDomain() {
-		return questionItemTypeImpl.getResponseDomain();
+		return doc.getQuestionItem().getResponseDomain();
 	}
 	
 	/**
@@ -329,10 +199,10 @@ public class QuestionItem extends Model {
 	 */
 	public RepresentationType setCodeResponseDomain(String codeSchemeReference) {
 
-		if (questionItemTypeImpl.getResponseDomain() != null) {
-			questionItemTypeImpl.unsetResponseDomain();
+		if (doc.getQuestionItem().getResponseDomain() != null) {
+			doc.getQuestionItem().unsetResponseDomain();
 		}
-		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		RepresentationType rt = doc.getQuestionItem().addNewResponseDomain();
 		CodeDomainType cdt = (CodeDomainType) rt.substitute(CodeDomainDocument.type.getDocumentElementName(),
 				CodeDomainType.type);
 		cdt.addNewCodeSchemeReference().addNewID().setStringValue(codeSchemeReference);
@@ -347,10 +217,10 @@ public class QuestionItem extends Model {
 	 */
 	public RepresentationType setTextResponseDomain(BigInteger maxLength) {
 
-		if (questionItemTypeImpl.getResponseDomain() != null) {
-			questionItemTypeImpl.unsetResponseDomain();
+		if (doc.getQuestionItem().getResponseDomain() != null) {
+			doc.getQuestionItem().unsetResponseDomain();
 		}
-		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		RepresentationType rt = doc.getQuestionItem().addNewResponseDomain();
 		TextDomainType tdt = (TextDomainType) rt.substitute(TextDomainDocument.type.getDocumentElementName(),
 				TextDomainType.type);
 		tdt.setMaxLength(maxLength);
@@ -366,10 +236,10 @@ public class QuestionItem extends Model {
 	 */
 	public RepresentationType setNumericResponseDomain(NumericTypeCodeType.Enum type, BigInteger decimalPosition) {
 
-		if (questionItemTypeImpl.getResponseDomain() != null) {
-			questionItemTypeImpl.unsetResponseDomain();
+		if (doc.getQuestionItem().getResponseDomain() != null) {
+			doc.getQuestionItem().unsetResponseDomain();
 		}
-		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		RepresentationType rt = doc.getQuestionItem().addNewResponseDomain();
 		NumericDomainType ndt = (NumericDomainType) rt.substitute(NumericDomainDocument.type
 				.getDocumentElementName(), NumericDomainType.type);
 		ndt.setType(type);
@@ -390,10 +260,10 @@ public class QuestionItem extends Model {
 	 */
 	public RepresentationType setResponseDomain(RESPONSE_TYPES responseType, String value) {
 
-		if (questionItemTypeImpl.getResponseDomain() != null) {
-			questionItemTypeImpl.unsetResponseDomain();
+		if (doc.getQuestionItem().getResponseDomain() != null) {
+			doc.getQuestionItem().unsetResponseDomain();
 		}
-		RepresentationType rt = questionItemTypeImpl.addNewResponseDomain();
+		RepresentationType rt = doc.getQuestionItem().addNewResponseDomain();
 		if (responseType == RESPONSE_TYPES.UNDEFINED) {
 			return rt;
 		} else if (responseType == RESPONSE_TYPES.CODE) {
@@ -428,28 +298,6 @@ public class QuestionItem extends Model {
 		}
 	}
 
-//	/**
-//	 * Get ID of Question Item.
-//	 * 
-//	 * @return ID string.
-//	 */
-//	public String getId() {
-//		return questionItemTypeImpl.getId();
-//	}
-//
-//	public boolean getIsVersionable() {
-//		return questionItemTypeImpl.getIsVersionable();
-//	}
-//
-//	/**
-//	 * Get version of Question Item.
-//	 * 
-//	 * @return version string
-//	 */
-//	public String getVersion() {
-//		return questionItemTypeImpl.getVersion();
-//	}
-
 	/**
 	 * Get Question Item Document of Question Item.
 	 * 
@@ -457,7 +305,7 @@ public class QuestionItem extends Model {
 	 */
 	@Override
 	public QuestionItemDocument getDocument() {
-		return questionItemDocument;
+		return doc;
 	}
 
 	/**
@@ -470,24 +318,33 @@ public class QuestionItem extends Model {
 		log.debug("Question Item validation performed");
 
 		// Check if a Response Domain has been given
-		if (questionItemTypeImpl.getResponseDomain() == null
-				|| questionItemTypeImpl.getResponseDomain().getClass().getSimpleName().equals("RepresentationTypeImpl")) {
+		if (doc.getQuestionItem().getResponseDomain() == null
+				|| doc.getQuestionItem().getResponseDomain().getClass().getSimpleName().equals("RepresentationTypeImpl")) {
 			throw new Exception(Messages.getString("QuestionItem.mess.QuestionResponseTypeHasNotBeenSpecified")); //$NON-NLS-1$
 		}
 		// Check if Language has been given for all Question Texts
-		for (Iterator iterator = questionItemLiteralTextList.iterator(); iterator.hasNext();) {
-			QuestionItemLiteralText questionItemLiteralText = (QuestionItemLiteralText) iterator.next();
-			if (questionItemLiteralText.getText().length() > 0 && questionItemLiteralText.getLanguageCode().length() == 0) {
+		DynamicTextType dynamicText = null;
+		List<DynamicTextType> dynamicTextList = doc.getQuestionItem().getQuestionTextList();
+		for (int i = 0; i < dynamicTextList.size(); i++) {
+			dynamicText = dynamicTextList.get(i);
+			if (dynamicText == null) {
+				continue;
+			}
+			if (XmlBeansUtil.getXmlAttributeValue(dynamicText.xmlText(), "lang=\"").equals("")) {
 				throw new Exception(Messages.getString("QuestionItem.mess.QuestionTextLanguageTypeHasNotBeenSpecified")); //$NON-NLS-1$
 			}
 		}
-
 		return; // No error found:
 	}
-
+	
 	@Override
 	public void executeChange(Object value, Class<?> type) throws Exception {
-		// TODO Auto-generated method stub
-		
+		// Set Concept reference
+		if (type.equals(ReferenceType.class)) {
+			ReferenceType ref = getConceptReferenceType();
+			ModelAccessor.setReference(ref, ((LightXmlObjectType) value));
+		} else {
+			log.debug("******** Class type not supported: "+type+" ********");
+		}
 	}
 }
