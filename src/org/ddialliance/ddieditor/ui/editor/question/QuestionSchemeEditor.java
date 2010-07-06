5a61c7969bfcfae04b3db028df8b7223c20a0b52
Dan Kristiansen <dak@dda.dk>
2010-07-06 09:33:22 +0000

    Code simplified.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1334 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/question/QuestionSchemeEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/question/QuestionSchemeEditor.java
@@ -9,26 +9,12 @@ package org.ddialliance.ddieditor.ui.editor.question;
 import java.text.MessageFormat;
 
 import org.ddialliance.ddieditor.ui.dbxml.question.QuestionSchemeDao;
-import org.ddialliance.ddieditor.ui.editor.EditorInput;
 import org.ddialliance.ddieditor.ui.editor.LabelDescriptionEditor;
-import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
-import org.ddialliance.ddieditor.ui.model.LabelDescription;
-import org.ddialliance.ddieditor.ui.model.question.QuestionScheme;
 import org.ddialliance.ddieditor.ui.perspective.QuestionsPerspective;
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
 
 /**
  * Question Scheme Editor
@@ -37,9 +23,6 @@ public class QuestionSchemeEditor extends LabelDescriptionEditor {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, QuestionSchemeEditor.class);
 	public static final String ID = "org.ddialliance.ddieditor.ui.editor.question.QuestionSchemeEditor";
 
-	// Member variables:
-	private QuestionScheme modelImpl;
-	private IEditorSite site;
 	public QuestionSchemeEditor() {
 		super(Messages
 				.getString("QuestionSchemeEditor.label.QuestionSchemeEditorLabel.QuestionSchemeEditor"), Messages
@@ -55,57 +38,5 @@ public class QuestionSchemeEditor extends LabelDescriptionEditor {
 	public String getPerspectiveSwitchDialogText() {
 		return MessageFormat.format(Messages.getString("perspective.switch.dialogtext"), Messages
 				.getString("perspective.questions"));
-	}
-
-	/**
-	 * Create contents of the editor part
-	 * 
-	 * @param parent
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		parent.setLayout(new GridLayout());
-		log.debug("QuestionSchemeEditor.createPartControl called");
-		super.createPartControl(parent);
-	}
-	
-	@Override
-	public void doSave(IProgressMonitor monitor) {
-		log.debug("QuestionSchemeEditor.doSave()");
-		super.doSave(monitor);
-
-		try {
-			modelImpl.validate();
-		} catch (Exception e1) {
-			String errMess = Messages.getString("QuestionSchemeEditor.mess.ValidationError"); //$NON-NLS-1$
-			ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-					ID, 0, errMess, null));
-			return;
-		}
-		try {
-			if (getEditorInputImpl().getEditorMode().equals(EditorModeType.NEW)) {
-				dao.create(modelImpl);
-				getEditorInputImpl().setEditorMode(EditorModeType.EDIT);
-			} else if (getEditorInputImpl().getEditorMode().equals(EditorModeType.EDIT)) {
-				dao.update(modelImpl);
-			} else if (getEditorInputImpl().getEditorMode().equals(EditorModeType.VIEW)) {
-				log.debug("*** Saved ignored! ***");
-			}
-		} catch (Exception e) {
-			String errMess = Messages.getString("QuestionSchemeEditor.mess.ErrorDuringSave"); //$NON-NLS-1$
-			ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-					ID, 0, errMess, e));
-			return;
-		}
-		updateParentView();
-		editorStatus.clearChanged();
-		log.debug("QuestionSchemeEditor.doSave(1): " + editorStatus.getStatus());
-	}
-	
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		
-		super.init(site, input);
-		log.debug("QuestionItemEditor.init()");
-		this.modelImpl = (QuestionScheme) model;
 	}
 }
