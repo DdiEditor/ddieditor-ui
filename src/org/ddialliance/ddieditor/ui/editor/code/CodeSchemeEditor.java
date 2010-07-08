edc8940f40f417a94ac6cce82f1993af788067a1
Dan Kristiansen <dak@dda.dk>
2010-07-08 11:33:59 +0000

    Minor update.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1349 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/code/CodeSchemeEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/code/CodeSchemeEditor.java
@@ -14,27 +14,12 @@ import java.text.MessageFormat;
 
 import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.dbxml.code.CodeSchemeDao;
-import org.ddialliance.ddieditor.ui.dbxml.concept.ConceptSchemeDao;
-import org.ddialliance.ddieditor.ui.editor.EditorInput;
 import org.ddialliance.ddieditor.ui.editor.LabelDescriptionEditor;
-import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
-import org.ddialliance.ddieditor.ui.model.LabelDescription;
-import org.ddialliance.ddieditor.ui.model.code.CodeScheme;
 import org.ddialliance.ddieditor.ui.perspective.CodesPerspective;
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
 
 public class CodeSchemeEditor extends LabelDescriptionEditor{
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, CodeSchemeEditor.class);
