e47c01a5d06330ca0d39f06a1fff64008316de13
Dan Kristiansen <dak@dda.dk>
2010-07-08 11:31:12 +0000

    Submenu is now only Code Scheme - not Code anymore.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1346 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/view/CodeView.java
+++ src/org/ddialliance/ddieditor/ui/view/CodeView.java
@@ -13,16 +13,10 @@ public class CodeView extends View {
 	public static final String ID = "org.ddialliance.ddieditor.ui.view.CodeView";
 
 	public CodeView() {
-		super(
-				ViewContentType.CodeContent,
-				Messages
-						.getString("CodeView.label.codeNavigationLabel.CodeNavigation"),
-				Messages
-						.getString("CodeView.label.selectLabel.NavigatorDescription"),
-				Messages.getString("CodeView.label.codeLabel.Code"),
-				ElementType.CODE_SCHEME,
-				Messages
-						.getString("CodeView.label.codeTreeGroup.CodeStructure"),
-				Arrays.asList(ElementType.CODE));
+		super(ViewContentType.CodeContent, Messages.getString("CodeView.label.codeNavigationLabel.CodeNavigation"),
+				Messages.getString("CodeView.label.selectLabel.NavigatorDescription"), Messages
+						.getString("CodeView.label.codeLabel.Code"), ElementType.CODE_SCHEME, Messages
+						.getString("CodeView.label.codeTreeGroup.CodeStructure"), Arrays
+						.asList(ElementType.CODE_SCHEME));
 	}
 }
