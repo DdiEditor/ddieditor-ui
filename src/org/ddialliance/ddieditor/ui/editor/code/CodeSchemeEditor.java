98ed6526954ac3ba1783fbacd531e9b66ff1a17f
Dan Kristiansen <dak@dda.dk>
2010-07-08 07:50:54 +0000

    First working version but editing of Code is not supported.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1337 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/code/CodeSchemeEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/code/CodeSchemeEditor.java
@@ -1,11 +1,24 @@
 package org.ddialliance.ddieditor.ui.editor.code;
 
+/**
+ * Code Scheme Editor.
+ * 
+ */
+/*
+ * $Author$ 
+ * $Date$ 
+ * $Revision$
+ */
+
 import java.text.MessageFormat;
 
-
+import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.dbxml.code.CodeSchemeDao;
+import org.ddialliance.ddieditor.ui.dbxml.concept.ConceptSchemeDao;
+import org.ddialliance.ddieditor.ui.editor.EditorInput;
 import org.ddialliance.ddieditor.ui.editor.LabelDescriptionEditor;
 import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
+import org.ddialliance.ddieditor.ui.model.LabelDescription;
 import org.ddialliance.ddieditor.ui.model.code.CodeScheme;
 import org.ddialliance.ddieditor.ui.perspective.CodesPerspective;
 import org.ddialliance.ddieditor.ui.view.Messages;
@@ -23,22 +36,16 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 
-/**
- * Code Scheme Editor
- */
 public class CodeSchemeEditor extends LabelDescriptionEditor{
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, CodeSchemeEditor.class);
 	public static final String ID = "org.ddialliance.ddieditor.ui.editor.code.CodeSchemeEditor";
 
-	// Member variables:
-	private CodeScheme codeScheme;
-	private IEditorSite site;
-	
 	public CodeSchemeEditor() {
 		super(Messages
 				.getString("CodeSchemeEditor.label.CodeSchemeEditorLabel.CodeSchemeEditor"), Messages
 				.getString("CodeSchemeEditor.label.useTheEditorLabel.Description"), Messages
 				.getString("CodeSchemeEditor.label.CodeSchemeTabItem"));
+		dao = (IDao) new CodeSchemeDao();
 	}
 
 	public String getPreferredPerspectiveId() {
@@ -50,97 +57,4 @@ public class CodeSchemeEditor extends LabelDescriptionEditor{
 				.getString("perspective.codes"));
 	}
 
-	/**
-	 * Create contents of the editor part
-	 * 
-	 * @param parent
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		parent.setLayout(new GridLayout());
-		log.debug("CodeSchemeEditor.createPartControl called");
-		super.createPartControl(parent);
-	}
-	
-	@Override
-	public void doSave(IProgressMonitor monitor) {
-		log.debug("CodeSchemeEditor.doSave()");
-		super.doSave(monitor);
-
-		try {
-			codeScheme.validate();
-		} catch (Exception e1) {
-			String errMess = Messages.getString("CodeSchemeEditor.mess.ValidationError"); //$NON-NLS-1$
-			ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-					ID, 0, errMess, null));
-			return;
-		}
-		try {
-			if (getEditorInputImpl().getEditorMode().equals(EditorModeType.NEW)) {
-				CodeSchemeDao.create(codeScheme);
-				getEditorInputImpl().setEditorMode(EditorModeType.EDIT);
-			} else if (getEditorInputImpl().getEditorMode().equals(EditorModeType.EDIT)) {
-				CodeSchemeDao.update(codeScheme);
-			} else if (getEditorInputImpl().getEditorMode().equals(EditorModeType.VIEW)) {
-				log.debug("*** Saved ignored! ***");
-			}
-		} catch (Exception e) {
-			String errMess = Messages.getString("CodeSchemeEditor.mess.ErrorDuringSave"); //$NON-NLS-1$
-			ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-					ID, 0, errMess, e));
-			return;
-		}
-		updateParentView();
-		editorStatus.clearChanged();
-		log.debug("CodeSchemeEditor.doSave(1): " + editorStatus.getStatus());
-	}
-	
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		
-		// Initialize Code Scheme Editor Part:
-		super.setInput(input);
-		if (log.isDebugEnabled()) {
-			log.debug("CodeSchemeEditor.init() - Name: " + getEditorInputImpl().getName());
-			log.debug("CodeSchemeEditor.init() - ID: " + getEditorInputImpl().getId());
-			log.debug("CodeSchemeEditor.init() - Parent ID: " + getEditorInputImpl().getParentId());
-			log.debug("CodeSchemeEditor.init() - Editor Mode: " + getEditorInputImpl().getEditorMode());
-		}
-
-		if (getEditorInputImpl().getEditorMode().equals(EditorModeType.NEW)) {
-			try {
-				codeScheme = CodeSchemeDao.createCodeScheme(getEditorInputImpl().getId(), getEditorInputImpl().getVersion(),
-						getEditorInputImpl().getParentId(), getEditorInputImpl().getParentVersion());
-			} catch (Exception e) {
-				log.error("CodeSchemeEditor.init(): " + e.getMessage());
-				String errMess = Messages.getString("CodeSchemeEditor.mess.ErrorDuringCreateNewCode"); //$NON-NLS-1$
-				ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-						ID, 0, errMess, e));
-				System.exit(0);
-			}
-		} else if (getEditorInputImpl().getEditorMode().equals(EditorModeType.EDIT)
-				|| getEditorInputImpl().getEditorMode().equals(EditorModeType.VIEW)) {
-			try {
-				codeScheme = CodeSchemeDao.getCodeScheme(getEditorInputImpl().getId(), getEditorInputImpl().getVersion(),
-						getEditorInputImpl().getParentId(), getEditorInputImpl().getParentVersion());
-			} catch (Exception e) {
-				String errMess = Messages.getString("CodeSchemeEditor.mess.GetCodeByIdError"); //$NON-NLS-1$
-				ErrorDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), null, new Status(IStatus.ERROR,
-						ID, 0, errMess, e));
-				System.exit(0);
-			}
-		} else {
-			String errMess = MessageFormat.format(
-					Messages.getString("CodeSchemeEditor.mess.UnknownEditorMode"), getEditorInputImpl().getEditorMode()); //$NON-NLS-1$
-			MessageDialog.openError(site.getShell(), Messages.getString("ErrorTitle"), errMess);
-			System.exit(0);
-		}
-		
-		// Initialize the Simple Editor Part with Code Scheme:
-		super.init(site, input);
-		
-		this.site = site;
-		setSite(site);
-		setInput(getEditorInputImpl());
-		setPartName(getEditorInputImpl().getId());
-	}
 }
