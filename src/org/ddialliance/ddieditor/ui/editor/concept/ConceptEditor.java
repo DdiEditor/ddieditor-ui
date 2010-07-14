d107ef615786cec67d6d1eada14c380060e42ff5
Dan Kristiansen <dak@dda.dk>
2010-07-14 10:50:08 +0000

    Minor update.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1358 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/concept/ConceptEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/concept/ConceptEditor.java
@@ -12,37 +12,18 @@ package org.ddialliance.ddieditor.ui.editor.concept;
 
 import java.text.MessageFormat;
 
-import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.dbxml.concept.ConceptDao;
-import org.ddialliance.ddieditor.ui.editor.EditorInput;
 import org.ddialliance.ddieditor.ui.editor.LabelDescriptionEditor;
-import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
-import org.ddialliance.ddieditor.ui.model.LabelDescription;
-import org.ddialliance.ddieditor.ui.model.concept.Concept;
 import org.ddialliance.ddieditor.ui.perspective.ConceptsPerspective;
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
 
 public class ConceptEditor extends LabelDescriptionEditor {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM,
 			ConceptEditor.class);
 	public static final String ID = "org.ddialliance.ddieditor.ui.editor.concept.ConceptEditor";
-
-	// Member variables:
-	private Concept modelImpl;
-	private IEditorSite site;
 
 	public ConceptEditor() {
 		super(
@@ -62,27 +43,5 @@ public class ConceptEditor extends LabelDescriptionEditor {
 		return MessageFormat.format(Messages
 				.getString("perspective.switch.dialogtext"), Messages
 				.getString("perspective.concepts"));
-	}
-
-	/**
-	 * Create contents of the editor part
-	 * 
-	 * @param parent
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		parent.setLayout(new GridLayout());
-		log.debug("ConceptEditor.createPartControl called");
-		super.createPartControl(parent);
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-
-		// Initialize the Simple Editor Part with Concept:
-		super.init(site, input);
-		log.debug("ConceptEditor.init()");
-		this.modelImpl = (Concept) model;
 	}
 }
