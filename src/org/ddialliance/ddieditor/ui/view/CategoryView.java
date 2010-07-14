8213a7c0c869109b36caeef1f09298e1a63f9205
Dan Kristiansen <dak@dda.dk>
2010-07-14 11:02:40 +0000

    Initial version.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1371 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/view/CategoryView.java
@@ -0,0 +1,27 @@
+package org.ddialliance.ddieditor.ui.view;
+
+/**
+ * Category View.
+ * 
+ */
+import java.util.Arrays;
+
+import org.ddialliance.ddieditor.ui.model.ElementType;
+
+public class CategoryView extends View {
+	public static final String ID = "org.ddialliance.ddieditor.ui.view.CategoryView";
+
+	public CategoryView() {
+		super(
+				ViewContentType.CategoryContent,
+				Messages
+						.getString("CategoryView.label.categoryNavigationLabel.CategoryNavigation"),
+				Messages
+						.getString("CategoryView.label.selectLabel.NavigatorDescription"),
+				Messages.getString("CategoryView.label.categoryLabel.Category"),
+				ElementType.CATEGORY_SCHEME,
+				Messages
+						.getString("CategoryView.label.categoryTreeGroup.CategoryStructure"),
+				Arrays.asList(ElementType.CATEGORY));
+	}
+}
