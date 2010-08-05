27456ba426c24cd19feede925f3fd913d4c3ec1a
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-08-05 13:28:58 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1449 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/command/DisplayNote.java
@@ -0,0 +1,46 @@
+package org.ddialliance.ddieditor.ui.command;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.ddialliance.ddieditor.ui.dialogs.DisplayNoteDialog;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PlatformUI;
+
+public class DisplayNote extends org.eclipse.core.commands.AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		StringBuffer fileData = new StringBuffer(1000);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(
+					"resources/release-note.txt"));
+
+			char[] buf = new char[1024];
+			int numRead = 0;
+			while ((numRead = reader.read(buf)) != -1) {
+				String readData = String.valueOf(buf, 0, numRead);
+				fileData.append(readData);
+				buf = new char[1024];
+			}
+		} catch (Exception e) {
+			throw new ExecutionException(e.getMessage());
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// do nothing
+			}
+		}
+
+		DisplayNoteDialog dialog = new DisplayNoteDialog(PlatformUI
+				.getWorkbench().getDisplay().getActiveShell(), fileData
+				.toString());
+		dialog.open();
+
+		return null;
+	}
+}
---
+++ src/org/ddialliance/ddieditor/ui/dialogs/DisplayNoteDialog.java
@@ -0,0 +1,32 @@
+package org.ddialliance.ddieditor.ui.dialogs;
+
+import org.ddialliance.ddieditor.ui.editor.Editor;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+
+public class DisplayNoteDialog extends Dialog {
+	String note;
+
+	public DisplayNoteDialog(Shell parentShell, String note) {
+		super(parentShell);
+		this.note = note;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// dialog setup
+		this.getShell().setText("Release note");
+
+		// group
+		Editor editor = new Editor();
+		Group group = editor.createGroup(parent, "Release note");
+		group.setLayoutData(new GridData(700, 400));
+
+		editor.createTextAreaInput(group, "Release note", note, null);
+		return null;
+	}
+}
