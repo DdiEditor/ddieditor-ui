9fd4390e63061ce70af6c5ca8659e9ce827e7feb
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-08-05 11:59:04 +0000

    Formating and import organize
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1437 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/view/CodeView.java
+++ src/org/ddialliance/ddieditor/ui/view/CodeView.java
@@ -7,16 +7,21 @@ package org.ddialliance.ddieditor.ui.view;
 import java.util.Arrays;
 
 import org.ddialliance.ddieditor.ui.model.ElementType;
-import org.eclipse.swt.widgets.Composite;
 
 public class CodeView extends View {
 	public static final String ID = "org.ddialliance.ddieditor.ui.view.CodeView";
 
 	public CodeView() {
-		super(ViewContentType.CodeContent, Messages.getString("CodeView.label.codeNavigationLabel.CodeNavigation"),
-				Messages.getString("CodeView.label.selectLabel.NavigatorDescription"), Messages
-						.getString("CodeView.label.codeLabel.Code"), ElementType.CODE_SCHEME, Messages
-						.getString("CodeView.label.codeTreeGroup.CodeStructure"), Arrays
-						.asList(ElementType.CODE_SCHEME));
+		super(
+				ViewContentType.CodeContent,
+				Messages
+						.getString("CodeView.label.codeNavigationLabel.CodeNavigation"),
+				Messages
+						.getString("CodeView.label.selectLabel.NavigatorDescription"),
+				Messages.getString("CodeView.label.codeLabel.Code"),
+				ElementType.CODE_SCHEME,
+				Messages
+						.getString("CodeView.label.codeTreeGroup.CodeStructure"),
+				Arrays.asList(ElementType.CODE_SCHEME));
 	}
 }
