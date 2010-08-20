c753abb6272bc5aab7db9a90740d0301e00b31eb
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-08-20 12:08:28 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1557 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/command/CloseOpenEditors.java
+++ src/org/ddialliance/ddieditor/ui/command/CloseOpenEditors.java
@@ -1,10 +1,10 @@
 package org.ddialliance.ddieditor.ui.command;
 
 import org.ddialliance.ddieditor.ui.editor.EditorInput;
+import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IEditorReference;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 public class CloseOpenEditors extends org.eclipse.core.commands.AbstractHandler {
@@ -15,17 +15,20 @@ public class CloseOpenEditors extends org.eclipse.core.commands.AbstractHandler 
 				.getEditorReferences();
 		String[] resourceIds = event.getParameter(this.getClass().getName()
 				+ ".resourceId").split(CommandHelper.DELIMTER);
+		EditorInput editorInput= null;
 		for (int i = 0; i < openEditors.length; i++) {
 			try {
 				for (int j = 0; j < resourceIds.length; j++) {
-					if (((EditorInput) openEditors[i].getEditorInput())
+					editorInput = ((EditorInput) openEditors[i].getEditorInput());
+					if (editorInput
 							.getResourceId().equals(resourceIds[j])) {
 						PlatformUI.getWorkbench().getActiveWorkbenchWindow()
 								.getActivePage().closeEditor(
 										openEditors[i].getEditor(false), true);
 					}	
 				}
-			} catch (PartInitException e) {
+			} catch (Exception e) {
+				new DDIFtpException(e);
 				throw new ExecutionException(e.getMessage(), e);
 			}
 		}
