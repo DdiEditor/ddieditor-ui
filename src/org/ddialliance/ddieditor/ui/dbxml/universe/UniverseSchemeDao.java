a0b9f82993114f74b40c2a0999f11b9853c9c045
Dan Kristiansen <dak@dda.dk>
2010-08-27 12:41:04 +0000

    Updates releated to MaintainableLabelQuery
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1587 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/dbxml/universe/UniverseSchemeDao.java
+++ src/org/ddialliance/ddieditor/ui/dbxml/universe/UniverseSchemeDao.java
@@ -1,14 +1,20 @@
 package org.ddialliance.ddieditor.ui.dbxml.universe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ddialliance.ddi3.xml.xmlbeans.conceptualcomponent.UniverseSchemeDocument;
+import org.ddialliance.ddi3.xml.xmlbeans.datacollection.QuestionSchemeDocument;
 import org.ddialliance.ddieditor.logic.identification.IdentificationManager;
 import org.ddialliance.ddieditor.model.DdiManager;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
+import org.ddialliance.ddieditor.persistenceaccess.maintainablelabel.MaintainableLabelQueryResult;
+import org.ddialliance.ddieditor.persistenceaccess.maintainablelabel.MaintainableLabelUpdateElement;
 import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.model.ElementType;
 import org.ddialliance.ddieditor.ui.model.IModel;
+import org.ddialliance.ddieditor.ui.model.question.QuestionScheme;
+import org.ddialliance.ddieditor.ui.model.studyunit.StudyUnit;
 import org.ddialliance.ddieditor.ui.model.universe.UniverseScheme;
 import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.ddialliance.ddiftp.util.log.Log;
@@ -18,16 +24,47 @@ import org.ddialliance.ddiftp.util.log.LogType;
 public class UniverseSchemeDao implements IDao {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM, UniverseSchemeDao.class);
 
+	/**
+	 * Create Universe Scheme object
+	 * 
+	 * @param id
+	 * @param version
+	 * @param parentId
+	 * @param parentVersion
+	 * @param schemeQueryResult
+	 *            (or null)
+	 * @return StudyUnit
+	 * @throws Exception
+	 */
+	static public UniverseScheme create(String id, String version,
+			String parentId, String parentVersion,
+			MaintainableLabelQueryResult maintainableLabelQueryResult)
+			throws Exception {
+
+		log.debug("UniverseSchemeDao.createUniverseScheme()");
+		
+		UniverseSchemeDocument doc = UniverseSchemeDocument.Factory.newInstance();
+		IdentificationManager.getInstance().addIdentification(
+				doc.addNewUniverseScheme(),
+				ElementType.getElementType("UniverseScheme").getIdPrefix(), null);
+		IdentificationManager.getInstance().addVersionInformation(doc.getUniverseScheme(), null, null);
+
+		UniverseScheme universeScheme = new UniverseScheme(doc.getUniverseScheme().getId(), doc.getUniverseScheme().getVersion(), parentId,
+				parentVersion, "TODO", maintainableLabelQueryResult);
+
+		return universeScheme;
+	}
+
 	@Override
 	public IModel create(String id, String version, String parentId, String parentVersion) throws Exception {
 
-		log.debug("UniverseSchemeDao.create()");
-		UniverseSchemeDocument doc = UniverseSchemeDocument.Factory.newInstance();
-		IdentificationManager.getInstance().addIdentification(doc.addNewUniverseScheme(),
-				ElementType.getElementType("UniverseScheme").getIdPrefix(), null);
-		IdentificationManager.getInstance().addVersionInformation(doc.getUniverseScheme(), null, null);
-		UniverseScheme universeScheme = new UniverseScheme(doc, parentId, parentVersion);
-		return universeScheme;
+//		log.debug("UniverseSchemeDao.create()");
+//		UniverseSchemeDocument doc = UniverseSchemeDocument.Factory.newInstance();
+//		IdentificationManager.getInstance().addIdentification(doc.addNewUniverseScheme(),
+//				ElementType.getElementType("UniverseScheme").getIdPrefix(), null);
+//		IdentificationManager.getInstance().addVersionInformation(doc.getUniverseScheme(), null, null);
+//		UniverseScheme universeScheme = new UniverseScheme(doc, parentId, parentVersion);
+		return create(id, version, parentId, parentVersion, new MaintainableLabelQueryResult());
 	}
 
 	@Override
@@ -69,10 +106,31 @@ public class UniverseSchemeDao implements IDao {
 	public UniverseScheme getModel(String id, String version, String parentId, String parentVersion) throws Exception {
 
 		log.debug("UniverseSchemeDao.getModel(" + id + ")");
-		UniverseSchemeDocument doc = DdiManager.getInstance().getUniverseScheme(id, version, parentId, parentVersion);
-		UniverseScheme model = new UniverseScheme(doc, parentId, parentVersion);
 
-		return model;
+		MaintainableLabelQueryResult maintainableLabelQueryResult = DdiManager
+		.getInstance().getUniverseSchemeLabel(id, version, parentId,
+				parentVersion);
+
+		UniverseScheme universeScheme = create(id, version, parentId,
+		parentVersion, maintainableLabelQueryResult);
+
+		return universeScheme;
+	}
+	
+	/**
+	 * 
+	 * Update DBXML Universe Scheme corresponding to the given UniverScheme instance
+	 * 
+	 * @param universeScheme
+	 * @throws DDIFtpException
+	 */
+	static public void update(UniverseScheme universeScheme) throws DDIFtpException {
+
+		// TODO Version Control - not supported
+		MaintainableLabelQueryResult universeSchemeQueryResult = universeScheme.getMaintainableLabelQueryResult();
+
+		DdiManager.getInstance().updateMaintainableLabel(universeSchemeQueryResult,
+				universeScheme.getUpdateElements());
 	}
 
 	@Override
