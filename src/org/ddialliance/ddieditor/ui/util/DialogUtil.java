1fe64234d53b371619165628fbbac547c43f4d0d
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-08-31 09:14:13 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1594 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/util/DialogUtil.java
+++ src/org/ddialliance/ddieditor/ui/util/DialogUtil.java
@@ -2,7 +2,6 @@ package org.ddialliance.ddieditor.ui.util;
 
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.DDIFtpException;
-import org.ddialliance.ddiftp.util.Translator;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
@@ -19,13 +18,16 @@ public class DialogUtil {
 	 *            interface between an editor part and the workbench
 	 * @param pluginId
 	 *            ID of plug in e.g. editor, view, perspective ...
+	 * @param title
+	 *            dialog title [if null arg then translated 'ErrorTitle' is
+	 *            supplied]
 	 * @param errorMessage
 	 *            error message to display
 	 * @param e
 	 *            exception or throwable
 	 */
-	public static void errorDialog(IEditorSite site, String pluginId, String title,
-			String errorMessage, Throwable e) {
+	public static void errorDialog(IEditorSite site, String pluginId,
+			String title, String errorMessage, Throwable e) {
 		errorDialog(site.getShell(), pluginId, title, errorMessage, e);
 	}
 
@@ -34,23 +36,25 @@ public class DialogUtil {
 	 * 
 	 * @param shell
 	 *            parent shell
-	 * 
 	 * @param pluginId
 	 *            ID of plug in e.g. editor, view, perspective ...
+	 * @param title
+	 *            dialog title [if null arg then translated 'ErrorTitle' is
+	 *            supplied]
 	 * @param errorMessage
 	 *            error message to display
 	 * @param e
 	 *            exception or throwable
 	 */
-	public static void errorDialog(Shell shell, String pluginId, String title, 
+	public static void errorDialog(Shell shell, String pluginId, String title,
 			String errorMessage, Throwable e) {
 		checkException(e);
-		if (title==null) {
+		if (title == null) {
 			title = Messages.getString("ErrorTitle");
 		}
-		
-		ErrorDialog.openError(shell, title, null,
-				new Status(IStatus.ERROR, pluginId, 0, errorMessage, e));
+
+		ErrorDialog.openError(shell, title, null, new Status(IStatus.ERROR,
+				pluginId, 0, errorMessage, e));
 	}
 
 	private static void checkException(Throwable e) {
@@ -59,9 +63,9 @@ public class DialogUtil {
 			new DDIFtpException(new Exception(e));
 		}
 	}
-	
+
 	public static boolean yesNoDialog(String title, String message) {
-		return MessageDialog.openConfirm(PlatformUI.getWorkbench()
-				.getDisplay().getActiveShell(), title, message);
+		return MessageDialog.openConfirm(PlatformUI.getWorkbench().getDisplay()
+				.getActiveShell(), title, message);
 	}
 }
