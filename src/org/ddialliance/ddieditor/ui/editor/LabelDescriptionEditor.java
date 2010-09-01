0b53128089d7d242404d2b66513c3f0475153e10
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-09-01 14:28:41 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1620 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/LabelDescriptionEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/LabelDescriptionEditor.java
@@ -1,15 +1,5 @@
 package org.ddialliance.ddieditor.ui.editor;
 
-/**
- * Generic Simple Editor (support for Label and Description).
- * 
- */
-/*
- * $Author$ 
- * $Date$ 
- * $Revision$
- */
-
 import org.ddialliance.ddieditor.ui.model.LabelDescription;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
@@ -23,24 +13,23 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 
+/**
+ * Generic editor supporting label and description editing on an element
+ */
 public class LabelDescriptionEditor extends Editor {
 	private static Log log = LogFactory.getLog(LogType.SYSTEM,
 			LabelDescriptionEditor.class);
 
-	// Member variables:
 	private String editorEntityName;
 	private LabelDescription simpleElement;
-	private IEditorSite site;
 	private TableViewer tableViewer;
-	
-	protected Group labelDescriptionTabGroup; // May be used for expanding Label
-	// Description Tab content
 
-	public LabelDescriptionEditor(String headerEditorTitle, String headerEditorDescr,
-			String editorEntityName) {
+	// May be used for expanding Label Description Tab content
+	protected Group labelDescriptionTabGroup;
 
+	public LabelDescriptionEditor(String headerEditorTitle,
+			String headerEditorDescr, String editorEntityName) {
 		super(headerEditorTitle, headerEditorDescr);
-
 		this.editorEntityName = editorEntityName;
 	}
 
@@ -52,7 +41,6 @@ public class LabelDescriptionEditor extends Editor {
 	@Override
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout());
-		log.debug("Editor.createPartControl called");
 		super.createPartControl(parent);
 
 		createTabFolder(getComposite_1());
@@ -65,18 +53,12 @@ public class LabelDescriptionEditor extends Editor {
 	}
 
 	public Viewer getViewer() {
-		log.debug("SimpleEditor.getViewer()");
 		return this.tableViewer;
 	}
 
 	public void init(IEditorSite site, IEditorInput input)
 			throws PartInitException {
-		// Initialize the Stander Editor Part:
 		super.init(site, input);
-		this.simpleElement = (LabelDescription )model;
-	}
-	
-	public void getList() {
-		//this.site.
+		this.simpleElement = (LabelDescription) model;
 	}
 }
