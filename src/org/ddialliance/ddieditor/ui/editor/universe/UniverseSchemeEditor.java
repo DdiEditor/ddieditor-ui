821eb6f7818b54aea062e6a26a8079190f5a8d8d
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-09-02 12:59:56 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1624 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/universe/UniverseSchemeEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/universe/UniverseSchemeEditor.java
@@ -1,16 +1,6 @@
 package org.ddialliance.ddieditor.ui.editor.universe;
 
-	/**
-	 * Universe Scheme Editor.
-	 * 
-	 */
-	/*
-	 * $Author: ddadak $ 
-	 * $Date: 2010-07-08 13:33:59 +0200 (Thu, 08 Jul 2010) $ 
-	 * $Revision: 1349 $
-	 */
-
-	import java.text.MessageFormat;
+import java.text.MessageFormat;
 
 import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.dbxml.universe.UniverseSchemeDao;
@@ -21,25 +11,32 @@ import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
 
-		public class UniverseSchemeEditor extends LabelDescriptionEditor {
-			private static Log log = LogFactory.getLog(LogType.SYSTEM, UniverseSchemeEditor.class);
-			public static final String ID = "org.ddialliance.ddieditor.ui.editor.universe.UniverseSchemeEditor";
+/**
+ * Universe Scheme Editor
+ */
+public class UniverseSchemeEditor extends LabelDescriptionEditor {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM,
+			UniverseSchemeEditor.class);
+	public static final String ID = "org.ddialliance.ddieditor.ui.editor.universe.UniverseSchemeEditor";
 
-			public UniverseSchemeEditor() {
-				super(Messages
-						.getString("UniverseSchemeEditor.label.universeSchemeEditorLabel.UniverseSchemeEditor"), Messages
-						.getString("UniverseSchemeEditor.label.useTheEditorLabel.Description"), Messages
+	public UniverseSchemeEditor() {
+		super(
+				Messages
+						.getString("UniverseSchemeEditor.label.universeSchemeEditorLabel.UniverseSchemeEditor"),
+				Messages
+						.getString("UniverseSchemeEditor.label.useTheEditorLabel.Description"),
+				Messages
 						.getString("UniverseSchemeEditor.label.UniverseSchemeTabItem"));
-				dao = (IDao) new UniverseSchemeDao();
-			}
+		dao = (IDao) new UniverseSchemeDao();
+	}
 
-			public String getPreferredPerspectiveId() {
-				return UniversePerspective.ID;
-			}
+	public String getPreferredPerspectiveId() {
+		return UniversePerspective.ID;
+	}
 
-			public String getPerspectiveSwitchDialogText() {
-				return MessageFormat.format(Messages.getString("perspective.switch.dialogtext"), Messages
-						.getString("perspective.universe"));
-			}
-
-		}
+	public String getPerspectiveSwitchDialogText() {
+		return MessageFormat.format(Messages
+				.getString("perspective.switch.dialogtext"), Messages
+				.getString("perspective.universe"));
+	}
+}
