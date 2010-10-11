9c05d732587702f90c8172d98862c2d692875f51
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-10-11 07:23:58 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1757 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- product.sh
+++ product.sh
@@ -1,25 +1,54 @@
 #! /bin/sh
 
 #
-# build products
+# build products and properties
 #
-
-# ddi-3-xmlbeans
+echo '--- ddi-3-xmlbeans ---'
 cd ../ddi-3-xmlbeans
 ant init
 
-# ddieditor-ui
+echo '--- ddieditor-ui ---'
 cd ../ddieditor-ui
 ant resource
 
-# ddadb
+echo '--- edit db connections ---'
 cd ../ddadb
-vi source/resources/dda-app.properties
+vi db.properties
+
+echo '--- copy journal-study-info-export setup ---'
+cd ../jounal-study-info-export
+ant hibernate-tool-deploy
+ant deploy-to-ddieditor-ui
+
+echo '--- check db connection ---'
+vi ../ddieditor-ui/bin/resources/hibernate/hibernate.cfg.xml
+
+#
+# version bump
+#
+echo '--- increment bundle versions ---'
+# lib-java
+vi ../lib-java/META-INF/MANIFEST.MF
+
+# util
+vi ..lib-java/META-INF/MANIFEST.MF
+
+# ddieditor
+
+# ddieditor-ui
+
+# ddadb
+
+# jounal-study-info-export
+
+# ddieditor-ui product bundle
+
+echo '--- svn commit incremented bundle versions ---'
 
 #
 # tag build in svn
 #
 
 #
-# copy stuff into right place in final productcd
+# copy stuff into right place in final product
 #
