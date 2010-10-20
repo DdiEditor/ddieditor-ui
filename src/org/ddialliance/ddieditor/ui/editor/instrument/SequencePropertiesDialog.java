1ae3afbff0f3c46d1c8c4537e11dc0d784be2abf
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-10-20 06:28:57 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1829 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/editor/instrument/SequencePropertiesDialog.java
@@ -0,0 +1,87 @@
+package org.ddialliance.ddieditor.ui.editor.instrument;
+
+import org.ddialliance.ddieditor.ui.editor.Editor;
+import org.ddialliance.ddieditor.ui.model.instrument.Sequence;
+import org.ddialliance.ddieditor.ui.model.translationdialoginput.DescriptionTdI;
+import org.ddialliance.ddieditor.ui.model.translationdialoginput.LabelTdI;
+import org.ddialliance.ddieditor.ui.util.DialogUtil;
+import org.ddialliance.ddieditor.ui.view.Messages;
+import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Text;
+
+public class SequencePropertiesDialog extends Dialog {
+	static Log log = LogFactory.getLog(LogType.SYSTEM,
+			SequencePropertiesDialog.class);
+	Editor editor;
+	Sequence modelImpl;
+	String title;
+
+	public SequencePropertiesDialog(Shell parentShell, String title,
+			Editor editor) {
+		super(parentShell);
+		this.title = title;
+		this.editor = editor;
+		this.modelImpl = (Sequence)editor.getModel();
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// dialog setup
+		this.getShell().setText(title);
+		
+		// group
+		parent.setLayout(new GridLayout());
+		parent.setLayoutData(new GridData(800, 400));
+		editor.createTabFolder(parent);
+		
+		// description tab
+		TabItem tabItem2 = editor.createTabItem(Messages
+				.getString("editor.label.description"));
+		Group group2 = editor.createGroup(tabItem2, Messages
+				.getString("editor.label.description"));
+
+		try {
+			Text txt = editor.createLabelInput(group2, Messages
+					.getString("editor.label.label"), modelImpl.getDocument()
+					.getSequence().getLabelList(), modelImpl.getDocument()
+					.getSequence().getId());
+
+			editor.createTranslation(group2, Messages
+					.getString("editor.button.translate"), modelImpl
+					.getDocument().getSequence().getLabelList(),
+					new LabelTdI(), "", txt);
+
+			StyledText styledText = editor.createStructuredStringInput(group2,
+					Messages.getString("editor.label.description"), modelImpl
+							.getDocument().getSequence().getDescriptionList(),
+					modelImpl.getDocument().getSequence().getId());
+			editor.createTranslation(group2, Messages
+					.getString("editor.button.translate"), modelImpl
+					.getDocument().getSequence().getDescriptionList(),
+					new DescriptionTdI(), "", styledText);
+		} catch (DDIFtpException e) {
+			DialogUtil
+					.errorDialog(editor.getEditorSite(), editor.ID, null, e.getMessage(), e);
+		}
+
+		// id tab
+		editor.createPropertiesTab(editor.getTabFolder());
+
+		// xml tab
+		editor.createXmlTab(modelImpl);
+		return null;
+	}
+
+}
