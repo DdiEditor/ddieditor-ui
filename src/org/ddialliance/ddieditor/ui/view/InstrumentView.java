74d2a31f4d590da5f32cbb043e4dd4b09027111d
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-10-20 06:30:56 +0000

    Added dnd drag support
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1830 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/view/InstrumentView.java
+++ src/org/ddialliance/ddieditor/ui/view/InstrumentView.java
@@ -1,6 +1,11 @@
 package org.ddialliance.ddieditor.ui.view;
 
+import org.ddialliance.ddieditor.ui.editor.instrument.SequenceDragListener;
+import org.ddialliance.ddieditor.ui.editor.widgetutil.LightXmlObjectTransfer;
 import org.ddialliance.ddieditor.ui.model.ElementType;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Composite;
 
 public class InstrumentView extends View {
 	public static final String ID = "org.ddialliance.ddieditor.ui.view.InstrumentView";
@@ -10,8 +15,21 @@ public class InstrumentView extends View {
 				ViewContentType.InstrumentationContent,
 				Messages.getString("InstrumentView.label.navigation"),
 				Messages.getString("InstrumentView.label.label.Instrument"),
-				Messages
-						.getString("InstrumentView.label.selectLabel.NavigatorDescription"),
+				Messages.getString("InstrumentView.label.selectLabel.NavigatorDescription"),
 				ElementType.CONTROL_CONSTRUCT_SCHEME, "");
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		// dnd
+		int operations = DND.DROP_MOVE | DND.DROP_COPY;
+		Transfer[] transferTypes = new Transfer[] { LightXmlObjectTransfer
+				.getInstance() };
+
+		// jface
+		this.treeViewer.addDragSupport(operations, transferTypes,
+				new SequenceDragListener(treeViewer));		
 	}
 }
