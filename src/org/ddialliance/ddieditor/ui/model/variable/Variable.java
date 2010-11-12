bdb645f0d112a6f956cc7980f85d6dae1b01ab45
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-11-12 08:46:52 +0000

    Dev work update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1924 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/model/variable/Variable.java
+++ src/org/ddialliance/ddieditor/ui/model/variable/Variable.java
@@ -1,17 +1,29 @@
 package org.ddialliance.ddieditor.ui.model.variable;
 
-import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.RepresentationType;
+import java.math.BigInteger;
+
+import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.CodeRepresentationType;
 import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.VariableDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.NameType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.NumericRepresentationType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.NumericTypeCodeType;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.RepresentationType;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
 import org.ddialliance.ddieditor.ui.model.Model;
 import org.ddialliance.ddieditor.ui.model.ModelAccessor;
 import org.ddialliance.ddieditor.ui.model.ModelIdentifingType;
 import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
 
 public class Variable extends Model {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM, Variable.class);
 	VariableDocument doc;
+	public static String[] NUMERIC_TYPES = new String[] { "BigInteger",
+			"Integer", "Long", "Short", "Decimal", "Float", "Double", "Count",
+			"Incremental" };
 
 	public Variable(VariableDocument doc, String parentId, String parentVersion) {
 		super(doc, parentId, parentVersion);
@@ -68,9 +80,42 @@ public class Variable extends Model {
 			return doc.getVariable().getVariableNameList().get(0);
 		}
 	}
-	
-	public RepresentationType getRepresentation() {
+
+	public org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.RepresentationType getRepresentation() {
 		return doc.getVariable().getRepresentation();
+	}
+
+	public RepresentationType getValueRepresentation() {
+		// LiteralTextType lTextType = (LiteralTextType) textType.substitute(
+		// LiteralTextDocument.type.getDocumentElementName(),
+		// LiteralTextType.type);
+		// lTextType.addNewText();
+		return doc.getVariable().getRepresentation().getValueRepresentation();
+	}
+
+	public ReferenceType getCodeRepresentationCodeSchemeReference() {
+		RepresentationType rep = getValueRepresentation();
+		if (!(rep instanceof CodeRepresentationType)) {
+			return null;
+		}
+
+		ReferenceType reference = ((CodeRepresentationType) rep)
+				.getCodeSchemeReference();
+		if (create && reference == null) {
+			reference = ((CodeRepresentationType) rep)
+					.addNewCodeSchemeReference();
+		}
+		return reference;
+	}
+
+	public BigInteger getNumericDecimalPosition() {
+		if (!(getValueRepresentation() instanceof NumericRepresentationType)) {
+			log.warn("Not setting NumericRepresentation.type as ValueRepresentation is of type: "
+					+ getValueRepresentation().getClass().getName());
+			return null;
+		}
+		return ((NumericRepresentationType) getValueRepresentation())
+				.getDecimalPositions();
 	}
 
 	@Override
@@ -96,6 +141,39 @@ public class Variable extends Model {
 		if (type.equals(ModelIdentifingType.Type_C.class)) {
 			ReferenceType ref = getUniverseReference();
 			ModelAccessor.setReference(ref, (LightXmlObjectType) value);
+		}
+
+		// code representation
+		// code scheme ref
+		if (type.equals(ModelIdentifingType.Type_D.class)) {
+			ReferenceType ref = getCodeRepresentationCodeSchemeReference();
+			if (ref == null) {
+				log.warn("Not setting CodeRepresentationCodeSchemeReference as ValueRepresentation is of type: "
+						+ getValueRepresentation().getClass().getName());
+				return;
+			}
+			ModelAccessor.setReference(ref, (LightXmlObjectType) value);
+		}
+
+		// numeric type
+		if (type.equals(ModelIdentifingType.Type_E.class)) {
+			if (!(getValueRepresentation() instanceof NumericRepresentationType)) {
+				log.warn("Not setting NumericRepresentation.type as ValueRepresentation is of type: "
+						+ getValueRepresentation().getClass().getName());
+				return;
+			}
+			((NumericRepresentationType) getValueRepresentation())
+					.setType((NumericTypeCodeType.Enum) value);
+		}
+
+		if (type.equals(ModelIdentifingType.Type_F.class)) {
+			if (!(getValueRepresentation() instanceof NumericRepresentationType)) {
+				log.warn("Not setting NumericRepresentation.type as ValueRepresentation is of type: "
+						+ getValueRepresentation().getClass().getName());
+				return;
+			}
+			((NumericRepresentationType) getValueRepresentation())
+					.setDecimalPositions((BigInteger) value);
 		}
 	}
 
