b71063c8f10de0ec4f5ab9e91195c32cbb1a8c62
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-12-02 08:45:31 +0000

    Added ddieditor-line
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@1972 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- quickStart.sh
+++ quickStart.sh
@@ -17,7 +17,12 @@ ant resource -f dda-build.xml
 echo '--- Resource ddieditor-spss ---'
 cd ../ddieditor-spss
 ant resource -f dda-build.xml
-ant  deploy-to-ddieditor-ui -f dda-build.xml 
+ant deploy-to-ddieditor-ui -f dda-build.xml
+
+echo '--- Resource ddieditor-line ---'
+cd ../ddieditor-line
+ant resource -f dda-build.xml
+ant deploy-to-ddieditor-ui -f dda-build.xml
 
 echo '--- Resource jounal-study-info-export ---'
 cd ../jounal-study-info-export
