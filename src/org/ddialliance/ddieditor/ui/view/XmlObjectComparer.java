d46763023d7d48baaaa914844975606cb186ffd7
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-01-07 12:38:18 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2078 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/view/XmlObjectComparer.java
@@ -0,0 +1,52 @@
+package org.ddialliance.ddieditor.ui.view;
+
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlCursor.TokenType;
+import org.apache.xmlbeans.XmlObject;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.jface.viewers.IElementComparer;
+
+public class XmlObjectComparer implements IElementComparer {
+	// static Log log = LogFactory.getLog(LogType.SYSTEM,
+	// XmlObjectComparer.class);
+
+	@Override
+	public boolean equals(Object a, Object b) {
+		if (a instanceof XmlObject && b instanceof XmlObject) {
+			boolean result = ((XmlObject) a).valueEquals((XmlObject) b);
+			// if (log.isDebugEnabled() && result) {
+			// log.debug(result + "\n" + a + "\n" + b);
+			// }
+			return result;
+		}
+		return a.equals(b);
+	}
+
+	private CharSequence id = "id";
+
+	@Override
+	public int hashCode(Object element) {
+		if (element instanceof XmlObject) {
+			int key = 0;
+			XmlCursor xmlCursor = ((XmlObject) element).newCursor();
+			while (key == 0 && !xmlCursor.toNextToken().isNone()) {
+				// log.debug("next " +
+				// xmlCursor.currentTokenType().toString());
+				if (xmlCursor.currentTokenType().equals(TokenType.ATTR)
+						&& xmlCursor.getName().getLocalPart().equals(id)) {
+					// log.debug("attr "
+					// + xmlCursor.getName().getLocalPart());
+
+					key = xmlCursor.getTextValue().hashCode();
+					// log.debug("Key: " + key);
+				}
+			}
+			xmlCursor.dispose();
+			return key;
+		} else {
+			return element.hashCode();
+		}
+	}
+}
