3a24e57445e42aaab7d1e450b91540dce5f9cf95
Dan Kristiansen <dak@dda.dk>
2011-01-18 09:47:17 +0000

    Code Scheme / Category editor provided.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2085 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/widgetutil/lightxmlobjectdnd/LightXmlObjectDragListener.java
+++ src/org/ddialliance/ddieditor/ui/editor/widgetutil/lightxmlobjectdnd/LightXmlObjectDragListener.java
@@ -4,6 +4,9 @@ import java.util.Iterator;
 
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
 import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.log.Log;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.dnd.DragSource;
@@ -14,6 +17,7 @@ import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
 public class LightXmlObjectDragListener implements DragSourceListener {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM, LightXmlObjectDragListener.class);
 	private final Viewer viewer;
 	String rcpPartId;
 
@@ -26,17 +30,18 @@ public class LightXmlObjectDragListener implements DragSourceListener {
 	@Override
 	public void dragStart(DragSourceEvent event) {
 		// do nothing
+		System.out.println("LightXmlObjectDragListener.dragStart()");
 	}
 
 	@Override
 	public void dragSetData(DragSourceEvent event) {
-		IStructuredSelection selection = (IStructuredSelection) viewer
-				.getSelection();
+		log.debug("Start");
+		IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
 
 		// set data on event
 		int[] indices = null;
-		if (LightXmlObjectTransfer.getInstance()
-				.isSupportedType(event.dataType)) {
+		if (LightXmlObjectTransfer.getInstance().isSupportedType(event.dataType)) {
+			log.debug("Is supported, size: " + selection.size());
 			Object control = ((DragSource) event.getSource()).getControl();
 
 			// indices
@@ -63,37 +68,44 @@ public class LightXmlObjectDragListener implements DragSourceListener {
 			}
 			// guard
 			else {
-				new DDIFtpException("Event source not surpported: "
-						+ control.getClass().getName(), new Throwable());
+				new DDIFtpException("Event source not surpported: " + control.getClass().getName(), new Throwable());
 				return;
 			}
 
 			// set event data
 			int count = 0;
 			LightXmlObjectTransferVO[] result = new LightXmlObjectTransferVO[indices.length];
+			log.debug("result: "+result.length);
 			for (Iterator iterator = selection.iterator(); iterator.hasNext(); count++) {
-				result[count] = new LightXmlObjectTransferVO(rcpPartId,
-						indices[count], (LightXmlObjectType) iterator.next());
+				log.debug("Setting data: "+iterator.toString());
+				try {
+					result[count] = new LightXmlObjectTransferVO(rcpPartId, indices[count],
+							(LightXmlObjectType) iterator.next());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				log.debug("is here ?");
 			}
+
 			event.data = result;
 		}
+		log.debug("Doing return ;- )");
 	}
 
 	@Override
 	public void dragFinished(DragSourceEvent event) {
 		// do nothing
+		System.out.println("LightXmlObjectDragListener.dragFinished()");
 	}
 
-	private void defineIndexOfSelectedTreeItems(TreeItem[] selectedItems,
-			int[] indices, TreeItem treeItem) {
+	private void defineIndexOfSelectedTreeItems(TreeItem[] selectedItems, int[] indices, TreeItem treeItem) {
 		for (int i = 0; i < treeItem.getItemCount(); i++) {
 			if (treeItem.getItems()[i].getItemCount() > 0) {
-				defineIndexOfSelectedTreeItems(selectedItems, indices,
-						treeItem.getItems()[i]);
+				defineIndexOfSelectedTreeItems(selectedItems, indices, treeItem.getItems()[i]);
 			}
 			for (int j = 0; j < selectedItems.length; j++) {
-				System.out.println(selectedItems[j] + " ~ "
-						+ treeItem.getItems()[i]);
+				System.out.println(selectedItems[j] + " ~ " + treeItem.getItems()[i]);
 				if (selectedItems[j].equals(treeItem.getItems()[i])) {
 					indices[j] = i;
 				}
