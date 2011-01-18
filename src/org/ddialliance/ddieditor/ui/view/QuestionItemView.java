a5ee6b30abb3246afd3e0cb3922642cabe52219e
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-01-18 13:32:48 +0000

    Added DND
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2103 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/view/QuestionItemView.java
+++ src/org/ddialliance/ddieditor/ui/view/QuestionItemView.java
@@ -1,32 +1,40 @@
 package org.ddialliance.ddieditor.ui.view;
 
+import org.ddialliance.ddieditor.ui.editor.widgetutil.lightxmlobjectdnd.LightXmlObjectDragListener;
+import org.ddialliance.ddieditor.ui.editor.widgetutil.lightxmlobjectdnd.LightXmlObjectTransfer;
+import org.ddialliance.ddieditor.ui.model.ElementType;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Composite;
+
 /**
  * Question Item View.
  * 
  */
-/*
- * $Author$ 
- * $Date$ 
- * $Revision$
- */
-
-import org.ddialliance.ddieditor.ui.model.ElementType;
-
 public class QuestionItemView extends View {
 	public static final String ID = "org.ddialliance.ddieditor.ui.view.QuestionItemView";
 
 	public QuestionItemView() {
 		super(
 				ViewContentType.QuestionContent,
-				Messages
-						.getString("QuestionItemView.label.questionItemNavigationLabel.QuestionItemNavigation"),
-				Messages
-						.getString("QuestionItemView.label.selectLabel.NavigatorDescription"),
-				Messages
-						.getString("QuestionItemView.label.questionItemLabel.QuestionItem"),
+				Messages.getString("QuestionItemView.label.questionItemNavigationLabel.QuestionItemNavigation"),
+				Messages.getString("QuestionItemView.label.selectLabel.NavigatorDescription"),
+				Messages.getString("QuestionItemView.label.questionItemLabel.QuestionItem"),
 				ElementType.QUESTION_SCHEME,
-				Messages
-						.getString("QuestionItemView.label.questionItemsTreeGroup.QuestionItemStructure"));
+				Messages.getString("QuestionItemView.label.questionItemsTreeGroup.QuestionItemStructure"));
 	}
 
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		// dnd
+		int operations = DND.DROP_MOVE | DND.DROP_COPY;
+		Transfer[] transferTypes = new Transfer[] { LightXmlObjectTransfer
+				.getInstance() };
+
+		// drag
+		this.treeViewer.addDragSupport(operations, transferTypes,
+				new LightXmlObjectDragListener(treeViewer, ID));
+	}
 }
