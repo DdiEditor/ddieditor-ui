46b66b6bb038365b9f089d38d782d856dae71c38
Dan Kristiansen <dak@dda.dk>
2011-01-18 09:49:16 +0000

    Code Scheme / Category editor provided.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2087 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/model/code/CodeScheme.java
+++ src/org/ddialliance/ddieditor/ui/model/code/CodeScheme.java
@@ -10,34 +10,89 @@ package org.ddialliance.ddieditor.ui.model.code;
  * $Revision$
  */
 
-import org.ddialliance.ddieditor.persistenceaccess.maintainablelabel.MaintainableLabelQueryResult;
-import org.ddialliance.ddieditor.ui.model.LabelDescriptionScheme;
+import java.util.List;
+
+import org.apache.xmlbeans.XmlObject;
+import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.CodeSchemeDocument;
+import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.CodeSchemeType;
+import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.CodeType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.SchemeReferenceType;
+import org.ddialliance.ddieditor.ui.model.IModel;
+import org.ddialliance.ddieditor.ui.model.Model;
 import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
+import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
 
-public class CodeScheme extends LabelDescriptionScheme {
-	private static Log log = LogFactory
-			.getLog(LogType.SYSTEM, CodeScheme.class);
-
-	private MaintainableLabelQueryResult maintainableLabelQueryResult;
+public class CodeScheme extends Model implements IModel {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM, CodeScheme.class);
+	private CodeSchemeDocument doc;
 
 	/**
 	 * Constructor
 	 * 
-	 * @param id
-	 * @param version
+	 * @param questionItemDocument
 	 * @param parentId
 	 * @param parentVersion
-	 * @param maintainableLabelQueryResult
-	 * @throws DDIFtpException
+	 * @throws Exception
 	 */
-	public CodeScheme(String id, String version, String parentId,
-			String parentVersion, String agency,
-			MaintainableLabelQueryResult maintainableLabelQueryResult)
-			throws DDIFtpException {
-		super(maintainableLabelQueryResult, parentId, parentVersion);
+	public CodeScheme(CodeSchemeDocument doc, String parentId, String parentVersion) throws Exception {
+
+		super(doc.getCodeScheme(), parentId, parentVersion);
+
+		if (doc == null) {
+			this.doc = CodeSchemeDocument.Factory.newInstance();
+			// add id and version
+			setId("");
+			setVersion("");
+		} else {
+			this.doc = doc;
+		}
+	}
+	
+	public SchemeReferenceType getCategorySchemeReference() {
+		return doc.getCodeScheme().getCategorySchemeReference();
+	}
+	
+	/**
+	 * 
+	 * @param index 
+	 * 		index in list of code
+	 * @param categoryReference
+	 * @param value
+	 */
+	public void addCode(int index, String categoryReference, String value) {
+		
+		if(doc.getCodeScheme() == null) {
+			doc.addNewCodeScheme();
+		}
+		
+		if ((index < 0) || 
+			(doc.getCodeScheme().getCodeList().size() == 0 && index != 0) || 
+			(doc.getCodeScheme().getCodeList().size() > index)) {
+			// TODO error reporting
+			System.out.println("Index error");
+		}
+		CodeType codeType = CodeType.Factory.newInstance();
+		codeType.addNewCategoryReference().addNewID().setStringValue(categoryReference);
+		codeType.setValue(value);
+		doc.getCodeScheme().getCodeList().add(index, codeType);
+	}
+	
+	public void addCode(String categoryReference, String value) {
+		int index = -1;
+		if (doc.getCodeScheme() == null) {
+			index = 0;
+		} else {
+			index = doc.getCodeScheme().getCodeList().size();
+		}
+		addCode(index, categoryReference, value);
+	}
+	
+	public List<CodeType> getCodes() {
+		return doc.getCodeScheme().getCodeList();
 	}
 
 	@Override
@@ -47,6 +102,22 @@ public class CodeScheme extends LabelDescriptionScheme {
 
 	@Override
 	public void executeChange(Object value, Class<?> type) throws Exception {
-		// not impemented
+		XmlObject xml = (XmlObject) value;
+		if (type.equals(ReferenceType.class)) {
+			SchemeReferenceType ref = doc.getCodeScheme().getCategorySchemeReference();
+			String id = XmlBeansUtil.getXmlAttributeValue(xml.xmlText(), "id=\"");
+			if (ref == null) {
+				doc.getCodeScheme().addNewCategorySchemeReference().addNewID().setStringValue(id);
+			} else {
+				ref.getIDList().get(0).setStringValue(id);
+			}
+		} else {
+			log.debug("******** Class type not supported: " + type + " ********");
+		}
+	}
+
+	@Override
+	public CodeSchemeDocument getDocument() throws DDIFtpException {
+		return doc;
 	}
 }
