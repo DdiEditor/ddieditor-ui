d7e212aba2c54ee980e5405bd75b3af386844fc8
Dan Kristiansen <dak@dda.dk>
2011-02-02 09:57:28 +0000

    Minor update.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2191 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/LabelTypeModyfiListener.java
+++ src/org/ddialliance/ddieditor/ui/editor/LabelTypeModyfiListener.java
@@ -1,19 +1,23 @@
 package org.ddialliance.ddieditor.ui.editor;
+
+import java.util.Iterator;
 import java.util.List;
 
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.LabelType;
 import org.ddialliance.ddieditor.ui.editor.Editor.EditorStatus;
+import org.ddialliance.ddieditor.ui.view.XmlObjectComparer;
 import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Text;
 
-public class LabelTypeModyfiListener  implements ModifyListener {
+public class LabelTypeModyfiListener implements ModifyListener {
 	private LabelType label;
 	private List<LabelType> list;
 	private EditorStatus editorStatus;
 
-	public LabelTypeModyfiListener(LabelType label, List<LabelType> list, EditorStatus editorStatus) {
+	public LabelTypeModyfiListener(LabelType label, List<LabelType> list,
+			EditorStatus editorStatus) {
 		this.label = label;
 		this.list = list;
 		this.editorStatus = editorStatus;
@@ -23,11 +27,31 @@ public class LabelTypeModyfiListener  implements ModifyListener {
 	public void modifyText(ModifyEvent e) {
 		editorStatus.setChanged();
 		Text text = (Text) e.getSource();
+		boolean isNew = (Boolean) text.getData(Editor.NEW_ITEM);
+
+		// remove from list
+		if (text.getText().equals("") && !isNew) {
+			XmlObjectComparer comparer = new XmlObjectComparer();
+			for (Iterator<LabelType> iterator = list.iterator(); iterator
+					.hasNext();) {
+				LabelType listLabel = iterator.next();
+				if (comparer.equals(listLabel, label)) {
+					iterator.remove();
+					break;
+				}
+			}
+			return;
+		}
+
+		// set text
 		XmlBeansUtil.setTextOnMixedElement(label, text.getText());
+
+		// add to list
 		if ((Boolean) text.getData(Editor.NEW_ITEM)) {
 			list.add(label);
 			for (LabelType test : list) {
-				if (XmlBeansUtil.getTextOnMixedElement(test).equals(text.getText())) {
+				if (XmlBeansUtil.getTextOnMixedElement(test).equals(
+						text.getText())) {
 					label = test;
 				}
 			}
