cc70ac446fe370eb0feafb64a36477a36c4d20c7
Dan Kristiansen <dak@dda.dk>
2011-02-02 09:57:39 +0000

    Minor update.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2192 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/StructuredStringTypeModyfiListener.java
+++ src/org/ddialliance/ddieditor/ui/editor/StructuredStringTypeModyfiListener.java
@@ -1,22 +1,24 @@
 package org.ddialliance.ddieditor.ui.editor;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.StructuredStringType;
 import org.ddialliance.ddieditor.ui.editor.Editor.EditorStatus;
+import org.ddialliance.ddieditor.ui.view.XmlObjectComparer;
 import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 
 public class StructuredStringTypeModyfiListener implements ModifyListener {
-	private StructuredStringType name;
+	private StructuredStringType strType;
 	private List<StructuredStringType> list;
 	private EditorStatus editorStatus;
 
-	public StructuredStringTypeModyfiListener(StructuredStringType name,
+	public StructuredStringTypeModyfiListener(StructuredStringType strType,
 			List<StructuredStringType> list, EditorStatus editorStatus) {
-		this.name = name;
+		this.strType = strType;
 		this.list = list;
 		this.editorStatus = editorStatus;
 	}
@@ -25,12 +27,31 @@ public class StructuredStringTypeModyfiListener implements ModifyListener {
 	public void modifyText(ModifyEvent e) {
 		editorStatus.setChanged();
 		StyledText text = (StyledText) e.getSource();
-		XmlBeansUtil.setTextOnMixedElement(name, text.getText());
-		if ((Boolean) text.getData(Editor.NEW_ITEM)) {
-			list.add(name);
+		boolean isNew = (Boolean) text.getData(Editor.NEW_ITEM);
+
+		// remove from list
+		if (text.getText().equals("") && !isNew) {			
+			XmlObjectComparer comparer = new XmlObjectComparer();
+			for (Iterator<StructuredStringType> iterator = list.iterator(); iterator.hasNext();) {
+				StructuredStringType listStr = iterator.next();			
+				if (comparer.equals(listStr, strType)) {
+					iterator.remove();
+					break;
+				}
+			}
+			return;
+		}
+
+		// set text
+		XmlBeansUtil.setTextOnMixedElement(strType, text.getText());
+
+		// add to list
+		if (isNew) {
+			list.add(strType);
 			for (StructuredStringType test : list) {
-				if (XmlBeansUtil.getTextOnMixedElement(name).equals(text.getText())) {
-					name = test;
+				if (XmlBeansUtil.getTextOnMixedElement(strType).equals(
+						text.getText())) {
+					strType = test;
 				}
 			}
 			text.setData(Editor.NEW_ITEM, Boolean.FALSE);
