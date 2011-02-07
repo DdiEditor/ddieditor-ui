db2013169587cfb4979e406498178d29eda417a5
Dan Kristiansen <dak@dda.dk>
2011-02-07 14:43:58 +0000

    Preference exitst for table font.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2214 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/preference/PreferenceConstants.java
+++ src/org/ddialliance/ddieditor/ui/preference/PreferenceConstants.java
@@ -11,6 +11,8 @@ public class PreferenceConstants {
 	// ddieditor-ui
 	public static final String DDIEDITORUI_LANGUAGE = "ddieditorui.language";
 	public static final String AUTO_CHANGE_PERSPECTIVE = "auto.change.perspective";
+	public static final String[][] FONT_SIZE_LIST = {{"Small", "1"}, {"Medium", "2"}, {"Large", "3"}};
+	public static final String FONT_SIZE_TABLE_SIZE = "ddieditorui.fontsize.table";
 
 	// ddieditor
 	public static final String DDIEDITOR_DBXML_HOME = "ddieditor.dbxml.home";
