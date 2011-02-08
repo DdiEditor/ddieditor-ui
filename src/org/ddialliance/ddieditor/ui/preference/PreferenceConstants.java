565f02fa5f9679dfcb127a2d7fcbf22da4969027
Dan Kristiansen <dak@dda.dk>
2011-02-08 08:02:39 +0000

    Minor update.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2218 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/preference/PreferenceConstants.java
+++ src/org/ddialliance/ddieditor/ui/preference/PreferenceConstants.java
@@ -1,5 +1,7 @@
 package org.ddialliance.ddieditor.ui.preference;
 
+import org.ddialliance.ddieditor.ui.view.Messages;
+
 /**
  * Constant definitions for plug-in preferences.
  */
@@ -11,7 +13,10 @@ public class PreferenceConstants {
 	// ddieditor-ui
 	public static final String DDIEDITORUI_LANGUAGE = "ddieditorui.language";
 	public static final String AUTO_CHANGE_PERSPECTIVE = "auto.change.perspective";
-	public static final String[][] FONT_SIZE_LIST = {{"Small", "1"}, {"Medium", "2"}, {"Large", "3"}};
+	public static final String[][] FONT_SIZE_LIST = {
+			{ Messages.getString("pref.fontsize.small"), "1" },
+			{ Messages.getString("pref.fontsize.medium"), "2" },
+			{ Messages.getString("pref.fontsize.large"), "3" } };
 	public static final String FONT_SIZE_TABLE_SIZE = "ddieditorui.fontsize.table";
 
 	// ddieditor
