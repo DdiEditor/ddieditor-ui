bf962b0929f41320f0e6eb8d7e9350548c1b21f3
Dan Kristiansen <dak@dda.dk>
2011-03-10 08:57:51 +0000

    Editor mode set to 'view'
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2385 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/universe/UniverseEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/universe/UniverseEditor.java
@@ -1,25 +1,34 @@
 package org.ddialliance.ddieditor.ui.editor.universe;
 
 import java.text.MessageFormat;
+import java.util.List;
 
 import org.ddialliance.ddieditor.ui.dbxml.IDao;
 import org.ddialliance.ddieditor.ui.dbxml.universe.UniverseDao;
+import org.ddialliance.ddieditor.ui.editor.EditorInput;
+import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
 import org.ddialliance.ddieditor.ui.editor.LabelDescriptionEditor;
+import org.ddialliance.ddieditor.ui.model.ElementType;
+import org.ddialliance.ddieditor.ui.model.universe.Universe;
 import org.ddialliance.ddieditor.ui.perspective.UniversePerspective;
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
 
-public class UniverseEditor  extends LabelDescriptionEditor {
-	private static Log log = LogFactory.getLog(LogType.SYSTEM, UniverseEditor.class);
+public class UniverseEditor extends LabelDescriptionEditor {
+	private static Log log = LogFactory.getLog(LogType.SYSTEM,
+			UniverseEditor.class);
 	public static final String ID = "org.ddialliance.ddieditor.ui.editor.universe.UniverseEditor";
 
 	public UniverseEditor() {
-		super(Messages
-				.getString("UniverseEditor.label.universeEditorLabel.UniverseEditor"), Messages
-				.getString("UniverseEditor.label.useTheEditorLabel.Description"), Messages
-				.getString("UniverseEditor.label.UniverseTabItem"), ID);
+		super(
+				Messages.getString("UniverseEditor.label.universeEditorLabel.UniverseEditor"),
+				Messages.getString("UniverseEditor.label.useTheEditorLabel.Description"),
+				Messages.getString("UniverseEditor.label.UniverseTabItem"), ID);
 		dao = (IDao) new UniverseDao();
 	}
 
@@ -28,7 +37,34 @@ public class UniverseEditor  extends LabelDescriptionEditor {
 	}
 
 	public String getPerspectiveSwitchDialogText() {
-		return MessageFormat.format(Messages.getString("perspective.switch.dialogtext"), Messages
-				.getString("perspective.universe"));
+		return MessageFormat.format(
+				Messages.getString("perspective.switch.dialogtext"),
+				Messages.getString("perspective.universe"));
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		super.init(site, input);
+		// TODO Only make Universe check if it is a DDA installation
+		// Check if Universe derives from Journal DB
+		EditorInput editorInput = (EditorInput) input;
+		if (editorInput.getParentElementType().equals(
+				ElementType.UNIVERSE_SCHEME)) {
+			System.out
+					.println("Check if this Univer is references by Study Unit");
+			try {
+				List<String> ids = ((Universe) model).getStudyUnitReference();
+				for (String id : ids) {
+					if (id.equals(editorInput.getId())) {
+						// if so - update not allowed in DDI Editor - disable
+						// update
+						editorInput.setEditorMode(EditorModeType.VIEW);
+					}
+				}
+			} catch (Exception e) {
+				new PartInitException(e.getMessage());
+			}
+		}
 	}
 }
