50a23cadfb30ea231c2e4cc183c442844ca3ed9b
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-07-20 12:19:30 +0000

    Added support for multiple question refs
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2650 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/editor/instrument/IfThenElseEditor.java
+++ src/org/ddialliance/ddieditor/ui/editor/instrument/IfThenElseEditor.java
@@ -2,32 +2,63 @@ package org.ddialliance.ddieditor.ui.editor.instrument;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlObject;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.ProgrammingLanguageCodeType;
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
+import org.ddialliance.ddieditor.logic.urn.ddi.ReferenceResolution;
 import org.ddialliance.ddieditor.model.DdiManager;
+import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectListDocument;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
+import org.ddialliance.ddieditor.ui.Activator;
 import org.ddialliance.ddieditor.ui.dbxml.instrument.IfThenElseDao;
 import org.ddialliance.ddieditor.ui.editor.Editor;
+import org.ddialliance.ddieditor.ui.editor.EditorInput;
+import org.ddialliance.ddieditor.ui.editor.EditorInput.EditorModeType;
 import org.ddialliance.ddieditor.ui.editor.widgetutil.genericmodifylistener.TextStyledTextModyfiListener;
 import org.ddialliance.ddieditor.ui.editor.widgetutil.referenceselection.ReferenceSelectionAdapter;
 import org.ddialliance.ddieditor.ui.editor.widgetutil.referenceselection.ReferenceSelectionCombo;
+import org.ddialliance.ddieditor.ui.editor.widgetutil.table.TableColumnSort;
+import org.ddialliance.ddieditor.ui.model.ElementType;
+import org.ddialliance.ddieditor.ui.model.IModel;
 import org.ddialliance.ddieditor.ui.model.ModelIdentifingType;
 import org.ddialliance.ddieditor.ui.model.instrument.IfThenElse;
 import org.ddialliance.ddieditor.ui.model.translationdialoginput.DescriptionTdI;
 import org.ddialliance.ddieditor.ui.model.translationdialoginput.LabelTdI;
 import org.ddialliance.ddieditor.ui.perspective.IAutoChangePerspective;
 import org.ddialliance.ddieditor.ui.util.DialogUtil;
+import org.ddialliance.ddieditor.ui.util.LanguageUtil;
+import org.ddialliance.ddieditor.ui.util.swtdesigner.ResourceManager;
 import org.ddialliance.ddieditor.ui.view.Messages;
+import org.ddialliance.ddieditor.ui.view.TreeMenuProvider;
 import org.ddialliance.ddiftp.util.DDIFtpException;
 import org.ddialliance.ddiftp.util.log.Log;
 import org.ddialliance.ddiftp.util.log.LogFactory;
 import org.ddialliance.ddiftp.util.log.LogType;
+import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
@@ -39,6 +70,7 @@ public class IfThenElseEditor extends Editor implements IAutoChangePerspective {
 			IfThenElseEditor.class);
 	private List<LightXmlObjectType> questionRefList;
 	private IfThenElse modelImpl;
+	private Table table;
 
 	public IfThenElseEditor() {
 		super(
@@ -92,23 +124,113 @@ public class IfThenElseEditor extends Editor implements IAutoChangePerspective {
 						getEditorIdentification()));
 
 		// if question ref
-		try {
-			questionRefList = DdiManager.getInstance()
-					.getQuestionItemsLight(null, null, null, null)
-					.getLightXmlObjectList().getLightXmlObjectList();
-		} catch (Exception e) {
-			DialogUtil.errorDialog(getSite().getShell(), ID, null,
-					e.getMessage(), e);
+		createLabel(group,
+				Messages.getString("IfThenElse.editor.ifquestionref"));
+		
+		table = new Table(group, SWT.VIRTUAL | SWT.FULL_SELECTION | SWT.MULTI
+				| SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		GridData fileTableGd = new GridData(// );
+				GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL
+						| GridData.VERTICAL_ALIGN_FILL | GridData.GRAB_VERTICAL);
+		table.setRedraw(true);
+		table.setLayoutData(fileTableGd);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(false);
+		fileTableGd.horizontalSpan = 1;
+		Editor.resizeTableFont(table);
+		
+		// file table header
+		String[] columnNames = { "" };
+
+		// create table columns
+		TableColumnSort sortListener = new TableColumnSort(table);
+		for (int i = 0; i < columnNames.length; i++) {
+			TableColumn column = new TableColumn(table, SWT.NONE);
+			column.setText(columnNames[i]);
+			column.addListener(SWT.Selection, sortListener);
+			column.setResizable(true);
+			column.setWidth(130);
 		}
-		ReferenceSelectionCombo questionRefSelectCombo = createRefSelection(
-				group, Messages.getString("IfThenElse.editor.ifquestionref"),
-				Messages.getString("IfThenElse.editor.ifquestionref"),
-				modelImpl.getIfQuestionReference(), questionRefList, false);
-		questionRefSelectCombo.addSelectionListener(Messages
-				.getString("IfThenElse.editor.ifquestionref"),
-				new ReferenceSelectionAdapter(questionRefSelectCombo,
-						modelImpl, ModelIdentifingType.Type_B.class,
-						getEditorIdentification()));
+
+		// quei ref table content
+		try {
+			updatequeiRefTable();
+		} catch (DDIFtpException e) {
+			Editor.showError(e, ID);
+		}
+
+
+		// quei ref table listeners
+		// popup menu
+		Menu menu = new Menu(table);
+
+		// menu add
+		final MenuItem addMenuItem = new MenuItem(menu, SWT.CASCADE);
+		addMenuItem.setSelection(true);
+		addMenuItem.setText(Messages.getString("View.label.addMenuItem.Add"));
+		addMenuItem.setImage(ResourceManager.getPluginImage(
+				Activator.getDefault(), "icons/new_wiz.gif"));
+		addMenuItem.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				popupMenuAction(PopupAction.ADD);
+			}
+		});
+
+		// menu edit
+		MenuItem editMenuItem = new MenuItem(menu, SWT.NONE);
+		editMenuItem
+				.setText(Messages.getString("View.label.editMenuItem.Edit"));
+		editMenuItem.setImage(ResourceManager.getPluginImage(
+				Activator.getDefault(), "icons/editor_area.gif"));
+		editMenuItem.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				popupMenuAction(PopupAction.EDIT);
+			}
+		});
+
+		// menu remove
+		final MenuItem removeMenuItem = new MenuItem(menu, SWT.NONE);
+		removeMenuItem.setText(Messages
+				.getString("View.label.removeMenuItem.Remove"));
+		removeMenuItem.setImage(ResourceManager.getPluginImage(
+				Activator.getDefault(), "icons/delete_obj.gif"));
+		removeMenuItem.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+				popupMenuAction(PopupAction.REMOVE);
+			}
+		});
+		table.setMenu(menu);
+
+		table.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				// do nothing
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// do noting
+			}
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				TableItem[] items = ((Table) e.getSource()).getSelection();
+				for (int i = 0; i < items.length; i++) {
+					LightXmlObjectType lightXmlObject = (LightXmlObjectType) items[i]
+							.getData();
+					try {
+						TreeMenuProvider.defineInputAndOpenEditor(ElementType
+								.getElementType(lightXmlObject.getElement()),
+								ElementType.QUESTION_SCHEME, lightXmlObject,
+								EditorModeType.EDIT,
+								((EditorInput) getEditorInput())
+										.getResourceId(), null);
+					} catch (DDIFtpException e1) {
+						showError(e1);
+					}
+				}
+			}
+		});
 
 		// then ref
 		List<LightXmlObjectType> controlConstructRefList = new ArrayList<LightXmlObjectType>();
@@ -186,11 +308,253 @@ public class IfThenElseEditor extends Editor implements IAutoChangePerspective {
 		editorStatus.clearChanged();
 	}
 
+	public void updatequeiRefTable() throws DDIFtpException {
+		String[] queiRefIds = new String[modelImpl.getIfQuestionReferences()
+				.size()];
+		if (queiRefIds.length == 0) {
+			return;
+		}
+		int count = 0;
+		for (ReferenceType ref : modelImpl.getIfQuestionReferences()) {
+			ReferenceResolution refS = new ReferenceResolution(ref);
+			queiRefIds[count] = refS.getId();
+			count++;
+		}
+
+		// clear items
+		TableItem[] items = null;
+		try {
+			items = table.getItems();
+			for (int i = 0; i < items.length; i++) {
+				items[i].dispose();
+			}
+		} catch (Exception e) {
+			// swt null items in table exception
+		}
+
+		// insert into table
+		for (int i = 0; i < queiRefIds.length; i++) {
+			tableAdd(queiRefIds[i]);
+		}
+	}
+
+	private void tableAdd(String id) {
+		LightXmlObjectListDocument lightXmlList = null;
+		try {
+			lightXmlList = DdiManager.getInstance().getQuestionItemsLight(id,
+					null, null, null);
+		} catch (Exception e) {
+			showError(e);
+		}
+		if (lightXmlList.getLightXmlObjectList().getLightXmlObjectList()
+				.isEmpty()) {
+			return;
+		}
+		LightXmlObjectType lightXmlObject = lightXmlList
+				.getLightXmlObjectList().getLightXmlObjectList().get(0);
+		TableItem item = new TableItem(table, SWT.NONE);
+		try {
+			if (lightXmlObject.getLabelList().size() > 0) {
+				item.setText(0, XmlBeansUtil
+						.getTextOnMixedElement((XmlObject) XmlBeansUtil
+								.getLangElement(
+										LanguageUtil.getDisplayLanguage(),
+										lightXmlObject.getLabelList())));
+			}
+		} catch (DDIFtpException e) {
+			showError(e);
+		} catch (Exception e) {
+			showError(e);
+		}
+		item.setData(lightXmlObject);
+	}
+
+	private enum PopupAction {
+		ADD, EDIT, REMOVE
+	};
+
+	private void popupMenuAction(PopupAction action) {
+		TableItem[] tableItems = table.getSelection();
+		// guard
+		if (tableItems.length <= 0 && !action.equals(PopupAction.ADD)) {
+			return;
+		}
+
+		boolean update = false;
+		// add
+		if (action.equals(PopupAction.ADD)) {
+			List<LightXmlObjectType> controlConstructRefList = new ArrayList<LightXmlObjectType>();
+			try {
+				controlConstructRefList = DdiManager.getInstance()
+						.getQuestionItemsLight(null, null, null, null)
+						.getLightXmlObjectList().getLightXmlObjectList();
+			} catch (Exception e) {
+				DialogUtil.errorDialog(getSite().getShell(), ID, null,
+						e.getMessage(), e);
+			}
+
+			// weed out prechoosen ones
+			for (Iterator<LightXmlObjectType> iterator = controlConstructRefList
+					.iterator(); iterator.hasNext();) {
+				LightXmlObjectType lightXmlObjectType = iterator.next();
+				for (ReferenceType ref : modelImpl.getIfQuestionReferences()) {
+					if (new ReferenceResolution(ref).getId().equals(
+							lightXmlObjectType.getId())) {
+						iterator.remove();
+						break;
+					}
+				}
+			}
+
+			// labelQueryResult = null;
+			MenuPopupAddDialog addDialog = new MenuPopupAddDialog(getSite()
+					.getShell(),
+					Messages.getString("IfThenElse.editor.ifquestionref"),
+					Messages.getString("IfThenElse.editor.ifquestionref"),
+					controlConstructRefList, modelImpl);
+			addDialog.open();
+
+			if (addDialog.getResult() != null) {
+				update = true;
+
+				// swt add
+				tableAdd(addDialog.getResult().getId());
+				table.redraw();
+
+				// xml add
+				modelImpl.addNewQuestionReference(addDialog.getResult());
+
+			}
+		}
+
+		// edit and remove
+		for (int i = 0; i < tableItems.length; i++) {
+			LightXmlObjectType selectedLightXmlObject = (LightXmlObjectType) tableItems[i]
+					.getData();
+			// edit
+			if (action.equals(PopupAction.EDIT)) {
+				try {
+					TreeMenuProvider.defineInputAndOpenEditor(
+							ElementType.getElementType(selectedLightXmlObject
+									.getElement()),
+							ElementType.QUESTION_SCHEME,
+							selectedLightXmlObject, EditorModeType.EDIT,
+							((EditorInput) getEditorInput()).getResourceId(),
+							null);
+				} catch (DDIFtpException e) {
+					showError(e);
+				}
+			}
+			// remove
+			else if (action.equals(PopupAction.REMOVE)) {
+				int count = 0;
+				for (Iterator<ReferenceType> iterator = modelImpl
+						.getIfQuestionReferences().iterator(); iterator
+						.hasNext(); count++) {
+					ReferenceType type = iterator.next();
+
+					// remove from xml
+					if (selectedLightXmlObject.getId().equals(
+							new ReferenceResolution(type).getId())) {
+						iterator.remove();
+
+						// remove from table
+						update = true;
+						for (int j = 0; j < tableItems.length; j++) {
+							if (((LightXmlObjectType) tableItems[i].getData())
+									.getId().equals(
+											selectedLightXmlObject.getId())) {
+								table.remove(j);
+								table.redraw();
+							}
+						}
+						break;
+					}
+				}
+			} else {
+				log.warn("Action not specified!");
+			}
+		}
+
+		// update
+		if (update) {
+			this.editorStatus.setChanged();
+		}
+	}
+
 	@Override
 	public String getPerspectiveSwitchDialogText() {
 		return MessageFormat.format(
 				Messages.getString("perspective.switch.dialogtext"),
 				Messages.getString("perspective.instruments"));
+	}
+
+	class MenuPopupAddDialog extends Dialog {
+		Log log = LogFactory.getLog(LogType.SYSTEM, MenuPopupAddDialog.class);
+		Combo combo;
+		ReferenceSelectionCombo selectCombo;
+		LightXmlObjectType result;
+		int beforeAfter;
+
+		String title;
+		String label;
+		List<LightXmlObjectType> refs;
+		IModel modelImpl;
+
+		public MenuPopupAddDialog(Shell parentShell, String title,
+				String label, List<LightXmlObjectType> refs, IModel modelImpl) {
+			super(parentShell);
+			this.title = title;
+			this.label = label;
+			this.refs = refs;
+			this.modelImpl = modelImpl;
+		}
+
+		@Override
+		protected Control createDialogArea(Composite parent) {
+			// dialog setup
+			this.getShell().setText(title);
+
+			// group
+			Editor editor = new Editor();
+			Group group = editor.createGroup(parent, label);
+			group.setLayoutData(new GridData(700, 75));
+
+			// selection
+			selectCombo = editor.createRefSelection(group,
+					Messages.getString("IfThenElse.editor.ifquestionref"),
+					Messages.getString("IfThenElse.editor.ifquestionref"),
+					ReferenceType.Factory.newInstance(), refs, false);
+			selectCombo.addSelectionListener(
+					Messages.getString("IfThenElse.editor.ifquestionref"),
+					new MenuPopupSelectionAdapter(this));
+
+			return null;
+		}
+
+		public LightXmlObjectType getResult() {
+			return selectCombo.getResult();
+		}
+	}
+
+	class MenuPopupSelectionAdapter implements SelectionListener {
+		MenuPopupAddDialog dialog;
+
+		public MenuPopupSelectionAdapter(MenuPopupAddDialog dialog) {
+			super();
+			this.dialog = dialog;
+		}
+
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			dialog.result = dialog.selectCombo.getResult();
+			dialog.beforeAfter = dialog.combo.getSelectionIndex();
+		}
+
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {
+			// do nothing
+		}
 	}
 
 }
