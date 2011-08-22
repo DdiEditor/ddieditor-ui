5aa6474c924f47bb5723ab6e3cd437cea36cabaa
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-08-22 09:38:24 +0000

    Fix of empty codescheme choice
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2743 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/org/ddialliance/ddieditor/ui/model/code/CodeScheme.java
+++ src/org/ddialliance/ddieditor/ui/model/code/CodeScheme.java
@@ -26,7 +26,8 @@ import org.ddialliance.ddiftp.util.log.LogType;
 import org.ddialliance.ddiftp.util.xml.XmlBeansUtil;
 
 public class CodeScheme extends Model implements IModel {
-	private static Log log = LogFactory.getLog(LogType.SYSTEM, CodeScheme.class);
+	private static Log log = LogFactory
+			.getLog(LogType.SYSTEM, CodeScheme.class);
 	private CodeSchemeDocument doc;
 
 	/**
@@ -37,7 +38,8 @@ public class CodeScheme extends Model implements IModel {
 	 * @param parentVersion
 	 * @throws Exception
 	 */
-	public CodeScheme(CodeSchemeDocument doc, String parentId, String parentVersion) throws Exception {
+	public CodeScheme(CodeSchemeDocument doc, String parentId,
+			String parentVersion) throws Exception {
 
 		super(doc.getCodeScheme(), parentId, parentVersion);
 
@@ -50,36 +52,37 @@ public class CodeScheme extends Model implements IModel {
 			this.doc = doc;
 		}
 	}
-	
+
 	public SchemeReferenceType getCategorySchemeReference() {
 		return doc.getCodeScheme().getCategorySchemeReference();
 	}
-	
+
 	/**
 	 * 
-	 * @param index 
-	 * 		index in list of code
+	 * @param index
+	 *            index in list of code
 	 * @param categoryReference
 	 * @param value
 	 */
 	public void addCode(int index, String categoryReference, String value) {
-		
-		if(doc.getCodeScheme() == null) {
+
+		if (doc.getCodeScheme() == null) {
 			doc.addNewCodeScheme();
 		}
-		
-		if ((index < 0) || 
-			(doc.getCodeScheme().getCodeList().size() == 0 && index != 0) || 
-			(doc.getCodeScheme().getCodeList().size() > index)) {
+
+		if ((index < 0)
+				|| (doc.getCodeScheme().getCodeList().size() == 0 && index != 0)
+				|| (doc.getCodeScheme().getCodeList().size() > index)) {
 			// TODO error reporting
 			System.out.println("Index error");
 		}
 		CodeType codeType = CodeType.Factory.newInstance();
-		codeType.addNewCategoryReference().addNewID().setStringValue(categoryReference);
+		codeType.addNewCategoryReference().addNewID()
+				.setStringValue(categoryReference);
 		codeType.setValue(value);
 		doc.getCodeScheme().getCodeList().add(index, codeType);
 	}
-	
+
 	public void addCode(String categoryReference, String value) {
 		int index = -1;
 		if (doc.getCodeScheme() == null) {
@@ -89,7 +92,7 @@ public class CodeScheme extends Model implements IModel {
 		}
 		addCode(index, categoryReference, value);
 	}
-	
+
 	public List<CodeType> getCodes() {
 		return doc.getCodeScheme().getCodeList();
 	}
@@ -103,18 +106,28 @@ public class CodeScheme extends Model implements IModel {
 	public void executeChange(Object value, Class<?> type) throws Exception {
 		XmlObject xml = (XmlObject) value;
 		if (type.equals(ReferenceType.class)) {
-			SchemeReferenceType ref = doc.getCodeScheme().getCategorySchemeReference();
-			String id = XmlBeansUtil.getXmlAttributeValue(xml.xmlText(), "id=\"");
+			SchemeReferenceType ref = doc.getCodeScheme()
+					.getCategorySchemeReference();
+			String id = XmlBeansUtil.getXmlAttributeValue(xml.xmlText(),
+					"id=\"");
 			if (ref == null) {
-				doc.getCodeScheme().addNewCategorySchemeReference().addNewID().setStringValue(id);
+				doc.getCodeScheme().addNewCategorySchemeReference().addNewID()
+						.setStringValue(id);
 			} else {
 				if (ref.getIDList().size() == 0) {
 					ref.addNewID();
 				}
-				ref.getIDList().get(0).setStringValue(id);
+				if (id == null) {
+					// remove ref
+					doc.getCodeScheme().unsetCategorySchemeReference();
+				} else {
+					// set ref
+					ref.getIDList().get(0).setStringValue(id);
+				}
 			}
 		} else {
-			log.debug("******** Class type not supported: " + type + " ********");
+			log.debug("******** Class type not supported: " + type
+					+ " ********");
 		}
 	}
 
