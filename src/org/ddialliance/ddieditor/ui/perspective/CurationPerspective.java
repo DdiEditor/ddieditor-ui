83d83aecf4a0a859531f71881e111ea62686db7c
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-11-10 09:28:33 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@2928 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/perspective/CurationPerspective.java
@@ -0,0 +1,28 @@
+package org.ddialliance.ddieditor.ui.perspective;
+
+import org.ddialliance.ddieditor.ui.view.CodeView;
+import org.ddialliance.ddieditor.ui.view.InfoView;
+import org.ddialliance.ddieditor.ui.view.QuestionItemView;
+import org.ddialliance.ddieditor.ui.view.VariableView;
+import org.ddialliance.ddieditor.ui.view.instrument.sequenceflow.SequenceflowView;
+import org.ddialliance.ddieditor.ui.view.variable.questionrelation.VariableQuestionView;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class CurationPerspective implements IPerspectiveFactory {
+	public static final String ID = "org.ddialliance.ddieditor.ui.perspective.CurationPerspective";
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		IFolderLayout leftFolder = layout.createFolder("topLeft",
+				IPageLayout.LEFT, 0.55f, layout.getEditorArea());
+		leftFolder.addView(InfoView.ID);
+		leftFolder.addView(VariableQuestionView.ID);
+		leftFolder.addView(SequenceflowView.ID);
+		leftFolder.addView(CodeView.ID);
+		leftFolder.addView(VariableView.ID);
+		leftFolder.addView(QuestionItemView.ID);
+		leftFolder.addView(IPageLayout.ID_PROBLEM_VIEW);
+	}
+}
