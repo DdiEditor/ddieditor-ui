ac4d326670261808489bf44d24f46bd558beab2e
Dan Kristiansen <dak@dda.dk>
2012-02-10 10:02:30 +0000

    Initial version.
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@3179 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ tag.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+#
+# version bump
+#
+# function versionbump()
+versionbump()
+{
+projectpath=$1
+echo $projectpath
+fullpath=../$projectpath/META-INF/MANIFEST.MF
+vi $fullpath
+svn ci $fullpath  -m 'Version bump'
+return;
+}
+
+echo '--- Increment bundle versions [Bundle-Version: ] in MANIFEST.MF files ---'
+echo '--- And commit version bump in SVN ---'
+echo 'Do you want to version bump: [yes|no]'
+read doversionbump
+if [ $doversionbump = 'yes' ]
+then
+versionbump lib-java
+versionbump util
+versionbump ddieditor
+versionbump ddieditor-ui
+versionbump ddadb
+versionbump jounal-study-info-export
+versionbump ddieditor-spss
+versionbump ddieditor-line
+versionbump ddieditor-spss-stat
+
+# ddieditor-ui product bundle
+vi OSGI-INF/l10n/bundle.properties
+svn ci OSGI-INF/l10n/bundle.properties -m 'Version bump'
+else
+echo 'No version bumping taking place'
+fi
+
+#
+# tag build in svn
+#
+echo '--- Tag build in SVN ---'
+echo 'Do you want to tag in SVN: [yes|no]'
+read svntag
+if [ $svntag = 'yes' ]
+then
+echo 'Submit version no. for SVN tag:'
+read version
+
+svnurl=svn://192.168.99.101
+memo='Dev release taging: $version'
+svn mkdir $svnurl/dda/tags/ddiftp/test-$version -m '$memo'
+
+# dbdda
+svn copy $svnurl/dda/trunk/ddadb $svnurl/dda/tags/ddiftp/test-$version/ddadb -m '$memo' 
+
+# jounal-study-info-export
+svn copy $svnurl/dda/trunk/jounal-study-info-export $svnurl/dda/tags/ddiftp/test-$version/jounal-study-info-export -m '$memo'
+
+# ddieditor-osiris-batch-convert
+svn copy $svnurl/dda/trunk/ddieditor-osiris-batch-convert $svnurl/dda/tags/ddiftp/test-$version/ddieditor-osiris-batch-convert -m '$memo'
+
+# ddieditor-osiris2ddi3
+svn copy $svnurl/dda/trunk/ddieditor-osiris2ddi3 $svnurl/dda/tags/ddiftp/test-$version/ddieditor-osiris2ddi3 -m '$memo'
+
+# ddiftp
+svn copy $svnurl/dda/trunk/ddiftp $svnurl/dda/tags/ddiftp/test-$version/ -m '$memo'
+
+else
+echo 'No SVN tagging taking place'
+fi
+
+#
+# End
+#
+echo '--- The end, thank you for flying DDA RCP build script today ---'
