256b3466870727d85bd9917f6b721e6d917e1dae
Jannik Vestergaard Jensen <jvj@dda.dk>
2012-02-23 09:53:03 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-ui@3190 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ src/org/ddialliance/ddieditor/ui/dialogs/ValidateDialog.java
@@ -0,0 +1,79 @@
+package org.ddialliance.ddieditor.ui.dialogs;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ddialliance.ddieditor.model.resource.DDIResourceType;
+import org.ddialliance.ddieditor.persistenceaccess.PersistenceManager;
+import org.ddialliance.ddieditor.ui.editor.Editor;
+import org.ddialliance.ddiftp.util.DDIFtpException;
+import org.ddialliance.ddiftp.util.Translator;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+public class ValidateDialog extends Dialog{
+	List<DDIResourceType> resources = new ArrayList<DDIResourceType>();
+	public DDIResourceType ddiResource;
+
+	public ValidateDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// dialog setup
+		Editor editor = new Editor();
+		Group group = editor.createGroup(parent,
+				Translator.trans("validation"));
+		group.setLayoutData(new GridData(500, 50));
+		this.getShell().setText(
+				Translator.trans("validation"));
+
+		// print resource
+		try {
+			resources = PersistenceManager.getInstance().getResources();
+		} catch (DDIFtpException e) {
+			String errMess = MessageFormat
+					.format(Translator
+							.trans("validation.choose.error"), e.getMessage()); //$NON-NLS-1$
+			MessageDialog.openError(PlatformUI.getWorkbench().getDisplay()
+					.getActiveShell(), Translator.trans("ErrorTitle"), errMess);
+		}
+		String[] comboOptions = new String[resources.size()];
+		for (int i = 0; i < comboOptions.length; i++) {
+			comboOptions[i] = resources.get(i).getOrgName();
+		}
+		editor.createLabel(group,
+				Translator.trans("validation.choose"));
+		final Combo resouceCombo = editor.createCombo(group, comboOptions);
+		if (comboOptions.length == 1) {
+			resouceCombo.select(0);
+			ddiResource = resources.get(0);
+		} else {
+			resouceCombo.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					int selection = ((Combo) e.getSource()).getSelectionIndex();
+					ddiResource = resources.get(selection);
+				}
+
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					// do nothing
+				}
+			});
+		}
+
+		return null;
+	}
+}
