8c8b04a713a0666b606ab6753c2575648b02af47
ddadak <ddadak@dda.dk>
2014-06-23 13:59:00 +0200

    Import update.

--- src/org/ddialliance/ddieditor/ui/util/PrintUtil.java
+++ src/org/ddialliance/ddieditor/ui/util/PrintUtil.java
@@ -1,24 +1,15 @@
 package org.ddialliance.ddieditor.ui.util;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 
-import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
-import org.apache.fop.apps.Fop;
-import org.apache.fop.apps.FopFactory;
-import org.apache.fop.apps.MimeConstants;
 
 import org.ddialliance.ddieditor.util.DdiEditorConfig;
 
